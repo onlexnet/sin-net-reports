180947b1d23f82514c4c52bb7e4ae8f3be24945a
root <root@Home1.localdomain>
2019-12-06 23:21:27 +0100

    Polish references

--- webapi/webapi/src/main/java/net/siudek/Program.java
+++ webapi/webapi/src/main/java/net/siudek/Program.java
@@ -1,8 +1,12 @@
 package net.siudek;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Program {
 
     public static void main(String[] args) {
-        
+        SpringApplication.run(Program.class, args);
     }
 }
