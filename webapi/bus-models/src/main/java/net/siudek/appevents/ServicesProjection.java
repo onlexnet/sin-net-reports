145a692a6bc2a8fccb27c4868f06d39036c894f2
root <root@Home1.localdomain>
2019-12-07 10:38:50 +0100

    Progressing

--- webapi/bus-models/src/main/java/net/siudek/appevents/ServicesProjection.java
+++ webapi/bus-models/src/main/java/net/siudek/appevents/ServicesProjection.java
@@ -1,10 +1,11 @@
 package net.siudek.appevents;
 
-import org.springframework.cloud.bus.event.RemoteApplicationEvent;
+/** Query, response and notification about 'ServicesProjection' */
+public abstract class ServicesProjection {
 
-public class ServicesViewChanged extends RemoteApplicationEvent {
-
+    public final class Changed {
     /** Unique type id to check serialization. */
     private static final long serialVersionUID = 300971291522288790L;
+    }
 
 }
