d18130aee1eb0605a9deecbe607cde80abdf0258
Ubuntu <siudeks@siudeksdevmachine.43t202pckkdedgyhbz44bqyqsg.ax.internal.cloudapp.net>
2020-03-08 22:05:47 +0000

    Better navigation

--- portal/src/App.tsx
+++ portal/src/App.tsx
@@ -10,11 +10,19 @@ import { Customers } from "./Customers";
 
 export const App: React.FunctionComponent = () => {
   return (
-      <Router>
-        <Route path="/" component={NavBasicExample} />
-        <Route path="/services" component={Services} />
-        <Route path="/customers" component={Customers} />
-        <Route path="/" component={Home} />
-      </Router>
+    <Router>
+      <div className="ms-Grid" dir="ltr">
+        <div className="ms-Grid-row">
+          <div className="ms-Grid-col ms-sm6 ms-md4 ms-lg2">
+            <Route path="/" component={NavBasicExample} />
+          </div>
+          <div className="ms-Grid-col ms-sm6 ms-md8 ms-lg10">
+            <Route path="/services" component={Services} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/" component={Home} />
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 };
