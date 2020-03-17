21d2b3e4b62f1fa468fd1e7117a0e4d60426df23
Ubuntu <siudeks@siudeks-dev-home.43t202pckkdedgyhbz44bqyqsg.ax.internal.cloudapp.net>
2020-03-17 21:06:34 +0000

    Introduce tests

---
+++ portal/src/services/Main.test.tsx
@@ -0,0 +1,30 @@
+import { shallow } from 'enzyme';
+import { Main } from '.';
+
+describe('<Main />', () => {
+  it('renders three <Main /> components', () => {
+    const wrapper = shallow(<Main />);
+    //expect(wrapper.find(Foo)).to.have.lengthOf(3);
+  });
+
+//   it('renders an `.icon-star`', () => {
+//     const wrapper = shallow(<MyComponent />);
+//     expect(wrapper.find('.icon-star')).to.have.lengthOf(1);
+//   });
+
+//   it('renders children when passed in', () => {
+//     const wrapper = shallow((
+//       <MyComponent>
+//         <div className="unique" />
+//       </MyComponent>
+//     ));
+//     expect(wrapper.contains(<div className="unique" />)).to.equal(true);
+//   });
+
+//   it('simulates click events', () => {
+//     const onButtonClick = sinon.spy();
+//     const wrapper = shallow(<Foo onButtonClick={onButtonClick} />);
+//     wrapper.find('button').simulate('click');
+//     expect(onButtonClick).to.have.property('callCount', 1);
+//   });
+});
