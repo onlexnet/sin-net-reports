790830dfbf7d023cb26e8512ce2bf9da7e8002fc
Ubuntu <siudeks@siudeks-dev-home.43t202pckkdedgyhbz44bqyqsg.ax.internal.cloudapp.net>
2020-03-17 21:02:17 +0000

    Poprawna zmiana czasu

--- portal/src/services/Main.tsx
+++ portal/src/services/Main.tsx
@@ -1,11 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Content } from "./Content";
 import { CommandBarBasicExample } from "./Commands";
+import { IconButton } from "office-ui-fabric-react/lib/Button";
+import { createSecureContext } from "tls";
 
 export const Main: React.FC<{}> = props => {
+  const [period, setPeriod] = useState(new Date());
+
   return (
     <>
       <CommandBarBasicExample />
+      <IconButton
+        iconProps={{ iconName: "DoubleChevronLeft" }}
+        title="Poprzedni miesiąc"
+        ariaLabel="<<"
+        onClick={() => {
+          const newPeriod = new Date(
+            period.getFullYear(),
+            period.getMonth() - 1
+          );
+          setPeriod(newPeriod);
+        }}
+      />
+      <IconButton
+        iconProps={{ iconName: "DoubleChevronRight" }}
+        title="Następny miesiąc"
+        ariaLabel=">>"
+        onClick={() => {
+          const newPeriod = new Date(
+            period.getFullYear(),
+            period.getMonth() + 1
+          );
+          setPeriod(newPeriod);
+        }}
+      />
+
+      {period.toLocaleString("default", {
+        month: "long",
+        year: "numeric"
+      })}
       <Content />
     </>
   );
