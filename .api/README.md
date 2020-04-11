222e2979895351fcfa9ec3951c1a81a6a255f27e
Slawomir Siudek <siudeks@gmail.com>
2020-04-11 15:27:31 +0000

    Add random data

---
+++ .api/README.md
@@ -0,0 +1,3 @@
+# Sgared definition of contracts
+
+Some contract definition is shared between libraries / services. The ownership and location should be transparent and independent from localication bot: producer and consumer, so sspecial folder is created as an equivalent of separated repository.
--- webapi/graphql/src/main/java/sinnet/Query.java
+++ webapi/graphql/src/main/java/sinnet/Query.java
@@ -1,6 +1,7 @@
 package sinnet;
 
 import java.util.Collections;
+import java.util.Random;
 
 import com.coxautodev.graphql.tools.GraphQLQueryResolver;
 
@@ -9,6 +10,9 @@ import org.springframework.stereotype.Component;
 /** FixMe. */
 @Component
 public class Query implements GraphQLQueryResolver {
+
+    /** FixMe. */
+    static final Random RANDOM = new Random();
 
     /**
      * FixMe.
@@ -20,4 +24,58 @@ public class Query implements GraphQLQueryResolver {
         return Collections.emptyList();
     }
 
+}
+
+/** FixMe. */
+class DummyData {
+
+    /** FixMe. */
+    static final Random RANDOM = new Random();
+
+    /**
+     * FixMe.
+     * @return TODO
+     */
+    public static String randomEmployeeName() {
+        var candidates = "Anna Radek Bartek Mikołaj".split(" ");
+        return candidates[RANDOM.nextInt(candidates.length)];
+    };
+
+    /**
+     * FixMe.
+     * @return TODO
+     */
+    public static String randomCustomerName() {
+        var candidates = "Renoma Zgoda Medmax OrtMed".split(" ");
+        return candidates[RANDOM.nextInt(candidates.length)];
+    }
+
+    /**
+     * FixMe.
+     * @return TODO
+     */
+    public static Integer randomDuration() {
+        final int maxHoursPerService = 8;
+        return RANDOM.nextInt(maxHoursPerService) + 1;
+    }
+
+    /**
+     * FixMe.
+     * @return TODO
+     */
+    public static Integer randomDistance() {
+        final int maxDistance = 50;
+        return RANDOM.nextInt(maxDistance);
+    }
+
+    /**
+     * FixMe.
+     * @return TODO
+     */
+    public String randomServiceDescription() {
+        var candidates = ("Poprawa danych,Wprowadzanie korekt,"
+                       + "Odtwarzanie danych,konfiguracja środowisk")
+                       .split(",");
+        return candidates[RANDOM.nextInt(candidates.length)];
+    }
 }
