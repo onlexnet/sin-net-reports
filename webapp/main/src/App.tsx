86c1c131dcfcf6e25f24465cdeea1116174c4ceb
Slawomir Siudek <siudeks@gmail.com>
2020-05-12 19:06:34 +0000

    WIP

--- webapp/main/src/App.tsx
+++ webapp/main/src/App.tsx
@@ -7,29 +7,49 @@ import { Customers } from "./Customers";
 import { Main } from "./services";
 import { Reports } from "./reports/Reports";
 
-import ApolloClient from 'apollo-boost';
+import ApolloClient from "apollo-boost";
 import { routing } from "./Routing";
 
+import { AzureAD, AuthenticationState, IAzureADFunctionProps } from "react-aad-msal";
+import { authProvider } from "./authProvider";
+
+import { basicReduxStore } from "./reduxStore";
+
+import GetAccessTokenButton from "./GetAccessTokenButton";
+
 const client = new ApolloClient({
-  uri: 'http://localhost:8080',
+  uri: "http://localhost:8080"
 });
 
 export const App: React.FC<{}> = () => {
+
+  const [login, setLogin] = React.useState();
+
   return (
-    <Router>
-      <div className="ms-Grid" dir="ltr">
-        <div className="ms-Grid-row">
-          <div className="ms-Grid-col ms-sm6 ms-md4 ms-lg2">
-            <Route path="/" component={NavBasicExample} />
-          </div>
-          <div className="ms-Grid-col ms-sm6 ms-md8 ms-lg10">
-            <Route path={routing.services} component={Main} />
-            <Route path={routing.customers} component={Customers} />
-            <Route path={routing.reports} component={Reports} />
-            <Route path="/" exact component={Home} />
-          </div>
-        </div>
-      </div>
-    </Router>
+    <AzureAD provider={authProvider} reduxStore={basicReduxStore}>
+      {({ accountInfo, authenticationState, error }: IAzureADFunctionProps) => {
+
+        return (
+          <>
+          <GetAccessTokenButton provider={authProvider} />
+          <Router>
+            <div className="ms-Grid" dir="ltr">
+              <div className="ms-Grid-row">
+                <div className="ms-Grid-col ms-sm6 ms-md4 ms-lg2">
+                  <Route path="/" component={NavBasicExample} />
+                </div>
+                <div className="ms-Grid-col ms-sm6 ms-md8 ms-lg10">
+                  <Route path={routing.services} component={Main} />
+                  <Route path={routing.customers} component={Customers} />
+                  <Route path={routing.reports} component={Reports} />
+                  <Route path="/" exact component={Home} />
+                </div>
+              </div>
+            </div>
+          </Router>
+          </>
+        );
+      }}
+    </AzureAD>
   );
 };
