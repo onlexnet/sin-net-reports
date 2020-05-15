30146e3adceab6c9b7513f8b7faa4b58b24dc82c
Slawomir Siudek <siudeks@gmail.com>
2020-05-15 20:43:28 +0000

    Remove unused files

--- webapp/src/AppInProgress.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const View: React.FC<{}> = props => {
-  return <div>Login in progress ...</div>;
-};
--- webapp/src/AppUnauthenticated.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Button } from "office-ui-fabric-react";
-
-export const View: React.FC<{ login: () => void }> = ({ login }) => {
-  return <Button onClick={login}>login</Button>;
-};
