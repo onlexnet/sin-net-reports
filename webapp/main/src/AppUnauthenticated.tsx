ceceaeaf2503206f245b9beb0cd3515887d66522
Slawomir Siudek <siudeks@gmail.com>
2020-05-16 19:23:39 +0000

    Change text to trigger builds

--- webapp/main/src/AppUnauthenticated.tsx
+++ webapp/main/src/AppUnauthenticated.tsx
@@ -2,5 +2,7 @@ import React from "react";
 import { Button } from "office-ui-fabric-react";
 
 export const View: React.FC<{ login: () => void }> = ({ login }) => {
-  return <Button onClick={login}>login</Button>;
+  return (
+    <Button onClick={login}>Zaloguj się do aplikacji ...</Button>
+  );
 };
