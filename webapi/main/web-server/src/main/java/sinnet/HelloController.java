dfdbf2ccdb42d84ea4ae3cadfaef59083a6554b0
Sławomir Siudek <siudeks@gmail.com>
2020-05-19 19:28:40 +0200

    14/integrate with azure sso (#16)
    Implementation

---
+++ webapi/main/web-server/src/main/java/sinnet/HelloController.java
@@ -0,0 +1,48 @@
+package sinnet;
+
+import java.security.Principal;
+
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.minidev.json.JSONArray;
+
+/** Fixme. */
+@RestController
+public class HelloController {
+
+    /**
+     * Fixme.
+     * @param principal fixme
+     * @return fixme
+     */
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloWorld(final Principal principal) {
+        var a = (OAuth2AuthenticationToken) principal;
+        var p = (DefaultOidcUser) a.getPrincipal();
+        var claimsUntyped = p.getIdToken().<JSONArray>getClaim("roles");
+        for (var key : claimsUntyped) {
+            var k = (String) key;
+            System.out.println(k);
+        }
+
+        return a.getName();
+    }
+
+    /**
+     * Fixme.
+     * @param principal fixme
+     * @return fixme
+     */
+    @RequestMapping(value = "/roles", method = RequestMethod.GET)
+    @ResponseBody
+    public String helloWorld2(final Principal principal) {
+        return principal.getName();
+    }
+
+}
---
+++ webapi/main/web-server/src/main/java/sinnet/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package sinnet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+
+/**
+ * Fixme.
+*/
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    /** Fixme. */
+    @Autowired
+    private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService;
+
+    /**
+     * Fixme.
+     * @param http fixme
+     */
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+
+            .antMatchers("/graphql").permitAll()
+            .antMatchers("/vendor/**").permitAll()
+            .antMatchers("/graphiql").permitAll()
+
+            .anyRequest().authenticated()
+            .and()
+            .oauth2Login()
+            .userInfoEndpoint()
+            .oidcUserService(oidcUserService);
+    }
+}
