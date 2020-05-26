65fafab3d6aa0a86f5901229736d2074ec75ead4
Slawomir Siudek <siudeks@gmail.com>
2020-05-26 20:36:45 +0000

    Remove unused imports

--- reports/src/main/java/net/respekto/Function.java
+++ reports/src/main/java/net/respekto/Function.java
@@ -1,19 +1,18 @@
 package net.respekto;
 
-import java.io.ByteArrayOutputStream;
-import java.util.*;
-import com.microsoft.azure.functions.annotation.*;
+import java.util.Optional;
 
-import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import com.microsoft.azure.functions.ExecutionContext;
+import com.microsoft.azure.functions.HttpMethod;
+import com.microsoft.azure.functions.HttpRequestMessage;
+import com.microsoft.azure.functions.HttpResponseMessage;
+import com.microsoft.azure.functions.HttpStatus;
+import com.microsoft.azure.functions.annotation.AuthorizationLevel;
+import com.microsoft.azure.functions.annotation.FunctionName;
+import com.microsoft.azure.functions.annotation.HttpTrigger;
 
-import lombok.Cleanup;
 import lombok.SneakyThrows;
 import lombok.val;
-
-import com.microsoft.azure.functions.*;
 
 /**
  * Azure Functions with HTTP Trigger.
