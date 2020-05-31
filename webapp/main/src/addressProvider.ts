56964db201ab13e3f6e2b072d2ae4a8e2f129e39
Slawomir Siudek <siudeks@gmail.com>
2020-05-31 19:30:30 +0000

    Add configuration for prod env

--- webapp/main/src/addressProvider.ts
+++ webapp/main/src/addressProvider.ts
@@ -1,6 +1,12 @@
-
 export const addressProvider = (): { host: string } => {
-    return {
-        host: "http://localhost:8080"
-    }
-}
+  switch (window.location.origin) {
+    case "https://raport.sin.net.pl":
+      return {
+        host: "https://raport.sin.net.pl",
+      };
+    default:
+      return {
+        host: "http://localhost:8080",
+      };
+  }
+};
