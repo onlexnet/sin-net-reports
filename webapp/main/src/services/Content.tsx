f49f432a0960931c220fcd6cdb61649ba2cea21b
Slawomir Siudek <siudeks@gmail.com>
2020-07-31 23:19:33 +0000

    From Component -> function

--- webapp/main/src/services/Content.tsx
+++ webapp/main/src/services/Content.tsx
@@ -75,199 +75,191 @@ export interface TypedColumn extends IColumn {
   fieldName: keyof IDocument;
 }
 
-export class Content extends React.Component<{}, IContentState> {
-  private _allItems: IDocument[];
+export const Content: React.FC<{}> = props => {
+  const _allItems: IDocument[] = _generateDocuments();
 
-  constructor(props: {}) {
-    super(props);
-
-    this._allItems = _generateDocuments();
-
-    const columns: TypedColumn[] = [
-      {
-        key: "column4",
-        name: "Pracownik",
-        fieldName: "modifiedBy",
-        minWidth: 70,
-        maxWidth: 90,
-        isResizable: true,
-        isCollapsible: true,
-        data: "string",
-        onColumnClick: this._onColumnClick,
-        onRender: (item: IDocument) => {
-          return <span>{item.modifiedBy}</span>;
-        },
-        isPadded: true
-      },
-      {
-        key: "column3",
-        name: "Data",
-        fieldName: "dateModifiedValue",
-        minWidth: 70,
-        maxWidth: 90,
-        isResizable: true,
-        onColumnClick: this._onColumnClick,
-        data: "number",
-        onRender: (item: IDocument) => {
-          return <span>{item.dateModified}</span>;
-        },
-        isPadded: true
-      },
-      {
-        key: "column2",
-        name: "Klient",
-        fieldName: "customer",
-        minWidth: 210,
-        maxWidth: 350,
-        isRowHeader: true,
-        isResizable: true,
-        isSorted: true,
-        isSortedDescending: false,
-        sortAscendingAriaLabel: "Sorted A to Z",
-        sortDescendingAriaLabel: "Sorted Z to A",
-        onColumnClick: this._onColumnClick,
-        data: "string",
-        isPadded: true
-      },
-      {
-        key: "column6",
-        name: "Usługa",
-        fieldName: "description",
-        minWidth: 300,
-        maxWidth: 300,
-        isResizable: true,
-        isCollapsible: true,
-        data: "number",
-        onColumnClick: this._onColumnClick
-      },
-      {
-        key: "column5",
-        name: "Czas",
-        fieldName: "duration",
-        minWidth: 70,
-        maxWidth: 90,
-        isResizable: true,
-        isCollapsible: true,
-        data: "number",
-        onColumnClick: this._onColumnClick,
-        onRender: (item: IDocument) => {
-          return <span>{item.duration}</span>;
-        }
-      },
-      {
-        key: "column7",
-        name: "Dojazd",
-        fieldName: "distance",
-        minWidth: 70,
-        maxWidth: 90,
-        isResizable: true,
-        isCollapsible: true,
-        data: "number",
-        onColumnClick: this._onColumnClick,
-        onRender: (item: IDocument) => {
-          return <span>{item.distance}</span>;
-        }
-      }
-    ];
-
-    this.state = {
-      items: this._allItems,
-      columns: columns,
-      isModalSelection: false,
-      announcedMessage: undefined
-    };
-  }
-
-  public render() {
-    const { columns, items, announcedMessage } = this.state;
-    const stackTokens: IStackTokens = { childrenGap: 40 };
-
-    return (
-      <>
-        <div className={classNames.controlWrapper}>
-          <Stack>
-            <Stack horizontal tokens={stackTokens}>
-              <Toggle
-                label="Tylko moje dane"
-                checked={false}
-                onChange={this._onChangeCompactMode}
-                onText="Compact"
-                offText="Normal"
-                styles={controlStyles}
-              />
-              <TextField label="Tylko dzień:" onChange={this._onChangeDay} styles={controlStyles} />
-              <TextField label="Kontrahent:" onChange={this._onChangeText} styles={controlStyles} placeholder="fragment nazwy kontahenta" />
-            </Stack>
-          </Stack>
-        </div>
-        {announcedMessage ? <Announced message={announcedMessage} /> : undefined}
-
-        <DetailsList
-          items={items}
-          compact={true}
-          columns={columns}
-          selectionMode={SelectionMode.none}
-          getKey={this._getKey}
-          setKey="none"
-          layoutMode={DetailsListLayoutMode.justified}
-          isHeaderVisible={true}
-          onItemInvoked={this._onItemInvoked}
-        />
-      </>
-    );
-  }
-
-  private _getKey(item: any, index?: number): string {
-    return item.key;
-  }
-
-  private _onChangeCompactMode = (ev: React.MouseEvent<HTMLElement>, checked?: boolean): void => {
-    // no-op
-  };
-
-  private _onChangeModalSelection = (ev: React.MouseEvent<HTMLElement>, checked?: boolean): void => {
-    this.setState({ isModalSelection: checked ?? false });
-  };
-
-  private _onChangeText = (ev: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, text?: string): void => {
-    this.setState({
-      items: text ? this._allItems.filter(i => i.customer.toLowerCase().indexOf(text) > -1) : this._allItems
-    });
-  };
-
-  private _onChangeDay = (ev: React.FormEvent, text?: string): void => {
-    this.setState({
-      items: text ? this._allItems.filter(it => it.dateModified.includes(text)) : this._allItems
-    });
-  };
-
-  private _onItemInvoked(item: any): void {
-    alert(`Item invoked: ${item.name}`);
-  }
-
-  private _onColumnClick = (ev: React.MouseEvent<HTMLElement>, column: IColumn): void => {
-    const { columns, items } = this.state;
+  const _onColumnClick = (column: IColumn, columns: TypedColumn[]): void => {
     const newColumns: IColumn[] = columns.slice();
     const currColumn: IColumn = newColumns.filter(currCol => column.key === currCol.key)[0];
     newColumns.forEach((newCol: IColumn) => {
       if (newCol === currColumn) {
         currColumn.isSortedDescending = !currColumn.isSortedDescending;
         currColumn.isSorted = true;
-        this.setState({
+        setState({
+          ...state,
           announcedMessage: `${currColumn.name} is sorted ${currColumn.isSortedDescending ? "descending" : "ascending"}`
         });
       } else {
         newCol.isSorted = false;
         newCol.isSortedDescending = true;
       }
-    });
-    const newItems = _copyAndSort(items, currColumn.fieldName!, currColumn.isSortedDescending);
-    this.setState({
-      columns: newColumns,
-      items: newItems
-    });
+    })
   };
+
+  const initialColumns: TypedColumn[] = [
+    {
+      key: "column4",
+      name: "Pracownik",
+      fieldName: "modifiedBy",
+      minWidth: 70,
+      maxWidth: 90,
+      isResizable: true,
+      isCollapsible: true,
+      data: "string",
+      onColumnClick: (ev, col) => _onColumnClick(col, state.columns),
+      onRender: (item: IDocument) => {
+        return <span>{item.modifiedBy}</span>;
+      },
+      isPadded: true
+    },
+    {
+      key: "column3",
+      name: "Data",
+      fieldName: "dateModifiedValue",
+      minWidth: 70,
+      maxWidth: 90,
+      isResizable: true,
+      onColumnClick: (ev, col) => _onColumnClick(col, state.columns),
+      data: "number",
+      onRender: (item: IDocument) => {
+        return <span>{item.dateModified}</span>;
+      },
+      isPadded: true
+    },
+    {
+      key: "column2",
+      name: "Klient",
+      fieldName: "customer",
+      minWidth: 210,
+      maxWidth: 350,
+      isRowHeader: true,
+      isResizable: true,
+      isSorted: true,
+      isSortedDescending: false,
+      sortAscendingAriaLabel: "Sorted A to Z",
+      sortDescendingAriaLabel: "Sorted Z to A",
+      onColumnClick: (ev, col) => _onColumnClick(col, state.columns),
+      data: "string",
+      isPadded: true
+    },
+    {
+      key: "column6",
+      name: "Usługa",
+      fieldName: "description",
+      minWidth: 300,
+      maxWidth: 300,
+      isResizable: true,
+      isCollapsible: true,
+      data: "number",
+      onColumnClick: (ev, col) => _onColumnClick(col, state.columns),
+    },
+    {
+      key: "column5",
+      name: "Czas",
+      fieldName: "duration",
+      minWidth: 70,
+      maxWidth: 90,
+      isResizable: true,
+      isCollapsible: true,
+      data: "number",
+      onColumnClick: (ev, col) => _onColumnClick(col, state.columns),
+      onRender: (item: IDocument) => {
+        return <span>{item.duration}</span>;
+      }
+    },
+    {
+      key: "column7",
+      name: "Dojazd",
+      fieldName: "distance",
+      minWidth: 70,
+      maxWidth: 90,
+      isResizable: true,
+      isCollapsible: true,
+      data: "number",
+      onColumnClick: (ev, col) => _onColumnClick(col, state.columns),
+      onRender: (item: IDocument) => {
+        return <span>{item.distance}</span>;
+      }
+    }
+  ];
+
+  const [state, setState] = React.useState({
+    items: _allItems,
+    columns: initialColumns,
+    isModalSelection: false,
+    announcedMessage: ""
+  });
+
+  const { columns, items, announcedMessage } = state;
+  const stackTokens: IStackTokens = { childrenGap: 40 };
+
+  return (
+    <>
+      <div className={classNames.controlWrapper}>
+        <Stack>
+          <Stack horizontal tokens={stackTokens}>
+            <Toggle
+              label="Tylko moje dane"
+              checked={false}
+              onText="Compact"
+              offText="Normal"
+              styles={controlStyles}
+            />
+            {/* <TextField label="Tylko dzień:" onChange={this._onChangeDay} styles={controlStyles} />
+            <TextField label="Kontrahent:" onChange={this._onChangeText} styles={controlStyles} /> */}
+            <TextField label="Tylko dzień:" styles={controlStyles} />
+            <TextField label="Kontrahent:" styles={controlStyles} />
+          </Stack>
+        </Stack>
+      </div>
+      {announcedMessage ? <Announced message={announcedMessage} /> : undefined}
+
+      <DetailsList
+        items={items}
+        compact={true}
+        columns={columns}
+        selectionMode={SelectionMode.none}
+        //getKey={this._getKey}
+        setKey="none"
+        layoutMode={DetailsListLayoutMode.justified}
+        isHeaderVisible={true}
+        // onItemInvoked={this._onItemInvoked}
+      />
+    </>
+  );
 }
+
+//   private _getKey(item: any, index ?: number): string {
+//   return item.key;
+// }
+
+//   private _onChangeModalSelection = (ev: React.MouseEvent<HTMLElement>, checked?: boolean): void => {
+//   this.setState({ isModalSelection: checked ?? false });
+// };
+
+//   private _onChangeText = (ev: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, text?: string): void => {
+//   this.setState({
+//     items: text ? this._allItems.filter(i => i.customer.toLowerCase().indexOf(text) > -1) : this._allItems
+//   });
+// };
+
+//   private _onChangeDay = (ev: React.FormEvent, text?: string): void => {
+//   this.setState({
+//     items: text ? this._allItems.filter(it => it.dateModified.includes(text)) : this._allItems
+//   });
+// };
+
+//   private _onItemInvoked(item: any): void {
+//   alert(`Item invoked: ${item.name}`);
+//   }
+
+// const newItems = _copyAndSort(items, currColumn.fieldName!, currColumn.isSortedDescending);
+// this.setState({
+//   columns: newColumns,
+//   items: newItems
+// });
+// };
+
 
 function _copyAndSort<T>(items: T[], columnKey: string, isSortedDescending?: boolean): T[] {
   const key = columnKey as keyof T;
