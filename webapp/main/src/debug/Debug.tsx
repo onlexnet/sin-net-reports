4e711f2d1af127dc40d0eaad560ceb5485c96023
Slawomir Siudek <siudeks@gmail.com>
2020-09-08 20:07:01 +0000

    WIP

--- webapp/main/src/debug/Debug.tsx
+++ webapp/main/src/debug/Debug.tsx
@@ -9,6 +9,11 @@ export const Debug: React.FC<Props> = () => {
   // const client = apolloClientFactory(accountInfo.jwtIdToken);
   // const { data, loading, error } = useUserContextQuery({ client });
   const { data, loading, error } = useUserContextQuery();
+
+  if (data) return (<>
+    {data.getPrincipal.name}
+  </>)
+
   return (
     <>
       <p>data: {String(data)}</p>
