c4bbc26ad3de4461f0ede14573db5a7f4cf86104
Slawomir Siudek <siudeks@gmail.com>
2020-09-15 18:19:57 +0000

    WIP

--- webapi/main/web-server/src/main/java/sinnet/WebSecurityConfig.java
+++ webapi/main/web-server/src/main/java/sinnet/WebSecurityConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+// import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
 /** Fixme. */
 @Configuration
@@ -31,10 +31,10 @@ public class WebSecurityConfig {
                         conf
                             // .antMatchers("/graphql/**").hasAuthority("SCOPE_Actions.Read")
                             .antMatchers("/graphql/**").permitAll()
-                            .antMatchers("/actuator/**").permitAll(); })
-                    .oauth2ResourceServer()
-                    .jwt()
-                    .jwtAuthenticationConverter(new JwtAuthenticationConverter());
+                            .antMatchers("/actuator/**").permitAll(); });
+                    // .oauth2ResourceServer()
+                    // .jwt()
+                    // .jwtAuthenticationConverter(new JwtAuthenticationConverter());
             }
         };
     }
