a6d29eb38d8616c7169d2f20a434601177c74a7a
Slawomir Siudek <siudeks@gmail.com>
2020-09-18 23:09:20 +0000

    WIP

--- webapi/main/domain-core/src/main/java/sinnet/action/ActionDbModel.java
+++ webapi/main/domain-core/src/main/java/sinnet/action/ActionDbModel.java
@@ -4,22 +4,27 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Data;
+import sinnet.Const;
 
 @Entity
 @Table(name = "action_db_model")
 @Data
 class ActionDbModel {
 
+
     @Id
     private UUID entityId;
 
     private String description;
     private String servicemanName;
+
+    @Column(name = "customer-name", nullable = false, length = Const.Customer.NAME_LENGTH)
     private String customerName;
     private LocalDate when;
     private Duration duration;
