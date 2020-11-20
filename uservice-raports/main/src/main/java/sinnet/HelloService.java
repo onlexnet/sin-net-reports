414fefcf6cc20ecc13898ac3969188834459e4c0
Slawomir Siudek <siudeks@gmail.com>
2020-11-20 20:47:40 +0100

    WIP

--- uservice-raports/main/src/main/java/sinnet/HelloService.java
+++ uservice-raports/main/src/main/java/sinnet/HelloService.java
@@ -1,17 +1,19 @@
 package sinnet;
 
 import javax.inject.Singleton;
-import sinnet.gen.*;
 
 import io.grpc.stub.StreamObserver;
+import sinnet.gen.HelloRequest;
+import sinnet.gen.GreeterGrpc;
+import sinnet.gen.HelloReply;
 
 @Singleton
 public class HelloService extends GreeterGrpc.GreeterImplBase {
 
     @Override
     public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
-        String name = request.getName();
-        String message = "Hello " + name;
+        var name = request.getName();
+        var message = "Hello " + name;
         responseObserver.onNext(HelloReply.newBuilder().setMessage(message).build());
         responseObserver.onCompleted();
     }
