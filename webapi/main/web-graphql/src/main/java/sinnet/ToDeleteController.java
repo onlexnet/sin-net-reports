cad7c977e2e4ae4ece87ce2fcf92367a96fd4a0e
Slawomir Siudek <siudeks@gmail.com>
2020-11-22 12:45:55 +0100

    Build fix

--- webapi/main/web-graphql/src/main/java/sinnet/ToDeleteController.java
+++ webapi/main/web-graphql/src/main/java/sinnet/ToDeleteController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/to-delete")
 public class ToDeleteController {
-    
+
     @GetMapping
     public String get() {
         return "OK2BG";
