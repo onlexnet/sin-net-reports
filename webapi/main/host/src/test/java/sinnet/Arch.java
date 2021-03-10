6b49609c8400d502f3b76e274ca6b2c5529ad1d9
Slawomir Siudek <siudeks@gmail.com>
2021-03-10 10:31:22 +0100

    WIP

--- webapi/main/host/src/test/java/sinnet/Arch.java
+++ webapi/main/host/src/test/java/sinnet/Arch.java
@@ -3,7 +3,6 @@ package sinnet;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.security.access.annotation.Secured;
 
 @AnalyzeClasses(packages = "sinnet.models")
 public class Arch {
