b415a27ac8f6d959c67940d50052f65262977289
Slawomir Siudek <siudeks@gmail.com>
2021-03-10 22:32:22 +0100

    Reformat

--- webapi/main/web-graphql/src/main/java/sinnet/gql/DataType.java
+++ webapi/main/web-graphql/src/main/java/sinnet/gql/DataType.java
@@ -17,59 +17,65 @@ import graphql.schema.GraphQLScalarType;
 /** Implementation for Date scalar type used in GQL. */
 @Configuration
 class DateType {
+
+  /**
+   * Adds Date type to GraphQL semantic.
+   *
+   * @return GQL Date type converter to LocalDate
+   */
+  @Bean
+  public GraphQLScalarType dateScalarType() {
+    return GraphQLScalarType
+          .newScalar()
+          .name("Date")
+          .description("Date value without time")
+          .coercing(new LocalDateCoercing())
+          .build();
+  }
+
+  /** Allows to operate on new type for GQL: Date. */
+  static final class LocalDateCoercing implements Coercing<LocalDate, String> {
+
+    /** The only one supported format of Date. */
+    private static DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+
     /**
-     * Adds Date type to GraphQL semantic.
-     * @return GQL Date type converter to LocalDate
+     * Serializes LocalDate to String.
+     *
+     * @param input LocalDate to serialize
+     * @return serialized value
+     * @throws CoercingSerializeException if value input can't be serialized
      */
-    @Bean
-    public GraphQLScalarType dateScalarType() {
-        return GraphQLScalarType
-            .newScalar()
-            .name("Date")
-            .description("Date value without time")
-            .coercing(new LocalDateCoercing())
-            .build();
+    @Override
+    public String serialize(Object input) throws CoercingSerializeException {
+      var typedInput = (LocalDate) input;
+      return typedInput.format(pattern);
     }
 
-    /** Allows to operate on new type for GQL: Date. */
-    static final class LocalDateCoercing implements Coercing<LocalDate, String> {
-        /** The only one supported format of Date. */
-        private static DateTimeFormatter pattern = DateTimeFormatter.ofPattern("yyyy/MM/dd");
-
-        /**
-         * Serializes LocalDate to String.
-         * @param input LocalDate to serialize
-         * @return serialized value
-         * @throws CoercingSerializeException if value input can't be serialized
-         */
-        @Override
-        public String serialize(Object input) throws CoercingSerializeException {
-            var typedInput = (LocalDate) input;
-            return typedInput.format(pattern);
-        }
-
-        /**
-         * Parses provided input as a string and returns LocalDate.
-         * @param input object that will be parsed to LocalDate
-         * @return Parsed LocalDate
-         * @throws CoercingParseValueException if value input can't be parsed
-         */
-        @Override
-        public LocalDate parseValue(Object input) throws CoercingParseValueException {
-            var inputAsString = input.toString();
-            return LocalDate.parse(inputAsString, pattern);
-        }
-
-        /**
-         * Parses provided StringValue input and returns LocalDate.
-         * @param input object that will be parsed to LocalDate
-         * @return Parsed LocalDate
-         * @throws CoercingParseLiteralException if value input can't be parsed
-         */
-        @Override
-        public LocalDate parseLiteral(Object input) throws CoercingParseLiteralException {
-            var inputAsString = ((StringValue) input).getValue();
-            return LocalDate.parse(inputAsString, pattern);
-        }
+    /**
+     * Parses provided input as a string and returns LocalDate.
+     *
+     * @param input object that will be parsed to LocalDate
+     * @return Parsed LocalDate
+     * @throws CoercingParseValueException if value input can't be parsed
+     */
+    @Override
+    public LocalDate parseValue(Object input) throws CoercingParseValueException {
+      var inputAsString = input.toString();
+      return LocalDate.parse(inputAsString, pattern);
     }
+
+    /**
+     * Parses provided StringValue input and returns LocalDate.
+     *
+     * @param input object that will be parsed to LocalDate
+     * @return Parsed LocalDate
+     * @throws CoercingParseLiteralException if value input can't be parsed
+     */
+    @Override
+    public LocalDate parseLiteral(Object input) throws CoercingParseLiteralException {
+      var inputAsString = ((StringValue) input).getValue();
+      return LocalDate.parse(inputAsString, pattern);
+    }
+  }
 }
