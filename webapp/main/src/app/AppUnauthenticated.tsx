05815b47db4704c5dd5dc5f8fde340cbf324389e
Slawomir Siudek <siudeks@gmail.com>
2021-04-14 18:48:45 +0200

    Add version

--- webapp/main/src/app/AppUnauthenticated.tsx
+++ webapp/main/src/app/AppUnauthenticated.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { PrimaryButton, Stack } from "office-ui-fabric-react";
+import { Label, PrimaryButton, Stack } from "office-ui-fabric-react";
+import packageJson from '../../package.json';
 
 export const View: React.FC<{ login: () => void }> = ({ login }) => {
   return (
     <Stack>
       <Stack.Item align="center">
         <PrimaryButton onClick={login}>Zaloguj się do aplikacji ...</PrimaryButton>
+        <Label>{packageJson.version}</Label>
       </Stack.Item>
     </Stack>
   );
