bfb9c9b58c6385ee7309dfdaf177d04729ea050b
Slawomir Siudek <siudeks@gmail.com>
2021-04-22 22:27:44 +0200

    Add version to main page

--- webapp/main/src/Home.tsx
+++ webapp/main/src/Home.tsx
@@ -1,8 +1,13 @@
 import React from "react";
+import packageJson from '../package.json';
+
 
 export const Home: React.FC<{}> = (props) => {
 
     return (
+        <div>
         <h1>Witaj w systemie ewidencji usług.</h1>
+        <p>version: {packageJson.version}</p>
+        </div>
     )
 }
