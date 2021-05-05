d9bf5c74219306f50733daadfbf92d2cc88cc01a
Slawomir Siudek <siudeks@gmail.com>
2021-05-05 22:33:27 +0200

    WIP

--- gqlapi/main/src/main/scala/sinnet/GreetingResource.scala
+++ gqlapi/main/src/main/scala/sinnet/GreetingResource.scala
@@ -1,18 +1,16 @@
 package sinnet
 
-import javax.ws.rs.{GET, Path, Produces}
-import javax.ws.rs.core.MediaType
-import javax.inject.Inject
 import io.quarkus.example.GreeterGrpc
 import io.quarkus.grpc.runtime.annotations.GrpcService
 
+import javax.inject.Inject
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
+
 @Path("/hello-resteasy")
 class GreetingResource {
-
-    @Inject
-    @GrpcService("hello")
-    var client: GreeterGrpc.GreeterFutureStub = _
-
 
     @GET
     @Produces(Array[String](MediaType.TEXT_PLAIN))
