36d5363a19c2e6de8a317eee69e95befc009cfef
Slawomir Siudek <siudeks@gmail.com>
2021-05-06 18:18:13 +0200

    Reformat

--- webapi/main/domain-api/src/main/java/sinnet/models/ActionDuration.java
+++ webapi/main/domain-api/src/main/java/sinnet/models/ActionDuration.java
@@ -6,33 +6,35 @@ import lombok.Getter;
 @EqualsAndHashCode
 public final class ActionDuration {
 
-    private static final ActionDuration EMPTY = new ActionDuration(0);
+  private static final ActionDuration EMPTY = new ActionDuration(0);
 
-    @Getter
-    private int value;
+  @Getter
+  private int value;
 
-    private ActionDuration(int valueInMins) {
-        var valueCandidate = valueInMins;
-        if (valueCandidate < 0) valueCandidate = 0;
-        this.value = valueCandidate;
+  private ActionDuration(int valueInMins) {
+    var valueCandidate = valueInMins;
+    if (valueCandidate < 0) {
+      valueCandidate = 0;
     }
+    this.value = valueCandidate;
+  }
 
-    public static ActionDuration of(int value) {
-        return new ActionDuration(value);
-    }
+  public static ActionDuration of(int value) {
+    return new ActionDuration(value);
+  }
 
-    public static ActionDuration empty() {
-        return EMPTY;
-    }
+  public static ActionDuration empty() {
+    return EMPTY;
+  }
 
-    @Override
-    public String toString() {
-        final int minutesPerHour = 60;
-        var minutes = value % minutesPerHour;
-        var hours = value / minutesPerHour;
-        final int minumuValueWithDoubleDigits = 10;
-        var minutesPrefix = minutes < minumuValueWithDoubleDigits ? ":0" : ":";
-        var minutesAsText = minutesPrefix + minutes;
-        return hours + minutesAsText;
-    }
+  @Override
+  public String toString() {
+    final int minutesPerHour = 60;
+    var minutes = value % minutesPerHour;
+    var hours = value / minutesPerHour;
+    final int minumuValueWithDoubleDigits = 10;
+    var minutesPrefix = minutes < minumuValueWithDoubleDigits ? ":0" : ":";
+    var minutesAsText = minutesPrefix + minutes;
+    return hours + minutesAsText;
+  }
 }
