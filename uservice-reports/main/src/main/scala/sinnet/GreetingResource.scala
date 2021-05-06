05fa64bee15193fda4e0c1f606fba8bc0fd4fe19
Sławomir Siudek <siudeks@gmail.com>
2021-05-06 17:25:54 +0200

    New webapi (#35)
    * Initial commit
    
    * Rename root subproject
    
    * ignore local builds
    
    * Not compilable test
    
    * Green gRPC test
    
    * WIP
    
    * Rename folder
    
    * WIP
    
    * Update azure-pipeline.yaml for Azure Pipelines
    
    * Update azure-pipeline.yaml for Azure Pipelines

---
+++ uservice-reports/main/src/main/scala/sinnet/GreetingResource.scala
@@ -0,0 +1,18 @@
+package sinnet
+
+import io.quarkus.example.GreeterGrpc
+import io.quarkus.grpc.runtime.annotations.GrpcService
+
+import javax.inject.Inject
+import javax.ws.rs.GET
+import javax.ws.rs.Path
+import javax.ws.rs.Produces
+import javax.ws.rs.core.MediaType
+
+@Path("/hello-resteasy")
+class GreetingResource {
+
+    @GET
+    @Produces(Array[String](MediaType.TEXT_PLAIN))
+    def hello() = "Hello RESTEasy"
+}
---
+++ uservice-reports/main/src/test/scala/sinnet/GreetingResourceTest.scala
@@ -0,0 +1,28 @@
+package sinnet
+
+import io.quarkus.test.junit.QuarkusTest
+import io.restassured.RestAssured.given
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.jupiter.api.Test
+import io.quarkus.grpc.runtime.annotations.GrpcService;
+import io.quarkus.example.GreeterGrpc;
+import io.quarkus.example.HelloRequest;
+import javax.inject.Inject
+import scala.annotation.meta.field
+import org.junit.jupiter.api.Assertions
+
+@QuarkusTest
+class GreetingResourceTest {
+    
+    @Inject
+    @GrpcService("hello")
+    var client: GreeterGrpc.GreeterBlockingStub = _
+
+    @Test
+    def testHelloEndpoint() = {
+        var request = HelloRequest.newBuilder().setName("Ala ma kota").build()
+        var a = client.sayHello(request)
+        Assertions.assertEquals(a.getMessage(), "Hello Ala ma kota")
+    }
+
+}
