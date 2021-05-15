d34827d90d2fc6d04e4c0ad914dc1395fea3a7ac
Slawomir Siudek <siudeks@gmail.com>
2021-05-15 21:40:07 +0200

    WIP

--- webapi/docker-build-before.sh
+++ webapi/docker-build-before.sh
@@ -2,7 +2,8 @@
 # Copy all known local files used by the build and being outside of Docker build folder to local subfolder
 # named .docker-externals so that later on Docker, having such files, will copy them to its original locations but
 # located inside docker build process.
+
 rm -rf .docker-externals
 mkdir .docker-externals
+
 cp -r ../uservice-reports/main/src/main/proto/ .docker-externals/proto/
-# ln -s  ../uservice-reports/main/src/main/proto .docker-externals
