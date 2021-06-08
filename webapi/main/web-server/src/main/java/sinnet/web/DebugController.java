f8eb5c495be55c8b72fc234c7dbe998a2b8f7d1b
Slawomir Siudek <siudeks@gmail.com>
2021-06-08 22:16:24 +0200

    Test endpoint for reports

--- webapi/main/web-server/src/main/java/sinnet/web/DebugController.java
+++ webapi/main/web-server/src/main/java/sinnet/web/DebugController.java
@@ -4,9 +4,10 @@ import java.util.concurrent.CompletionStage;
 
 import javax.annotation.PostConstruct;
 
-import com.google.common.util.concurrent.Futures;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +18,10 @@ import io.grpc.ManagedChannelBuilder;
 import io.quarkus.example.GreeterGrpc;
 import io.quarkus.example.HelloRequest;
 import sinnet.FutureExecutor;
+import sinnet.reports.ActionDetails;
+import sinnet.reports.CustomerDetails;
+import sinnet.reports.ReportRequest;
+import sinnet.reports.ReportsGrpc;
 
 /**
  * Test controller to test calls endpoint -> webapi -> backend service using gRPC api.
@@ -30,6 +35,7 @@ import sinnet.FutureExecutor;
 class DebugController {
 
   private GreeterGrpc.GreeterFutureStub client;
+  private ReportsGrpc.ReportsFutureStub reportsClient;
 
   @Autowired
   private FutureExecutor executor;
@@ -39,7 +45,9 @@ class DebugController {
     var channel = ManagedChannelBuilder.forAddress("uservice-reports", 9000)
         .usePlaintext()
         .build();
+
     this.client = GreeterGrpc.newFutureStub(channel);
+    this.reportsClient = ReportsGrpc.newFutureStub(channel);
   }
 
   @RequestMapping(value = "/hello-by-service", method = RequestMethod.GET)
@@ -55,5 +63,38 @@ class DebugController {
   public String helloByWebapi(@RequestParam(defaultValue = "no-name") String name) {
     return "Hello from WebApi, " + name;
   }
+
+  @RequestMapping(value = "/report", method = RequestMethod.GET, produces = "application/pdf")
+  public CompletionStage<ResponseEntity<byte[]>> downloadPDFFile() {
+    var headers = new HttpHeaders();
+    headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+    headers.add("Content-Disposition", "inline; filename=test-report.zip");
+    headers.add("Pragma", "no-cache");
+    headers.add("Expires", "0");
+
+    var customer = CustomerDetails.newBuilder()
+        .setCustomerAddress("Adres")
+        .setCustomerCity("Miasto")
+        .setCustomerId("ID klienta")
+        .setCustomerName("Nazwa klienta")
+        .build();
+    var action = ActionDetails.newBuilder()
+        .setDescription("Opis usługi")
+        .build();
+    var request = ReportRequest.newBuilder()
+        .setCustomer(customer)
+        .addDetails(action)
+        .build();
+    var response = reportsClient.produce(request);
+    return executor.asFuture(response, it -> {
+      var result = it.getData().toByteArray();
+      return ResponseEntity.ok()
+        .headers(headers)
+        .contentLength(result.length)
+        .contentType(MediaType.parseMediaType("application/octet-stream"))
+        .body(result);
+    });
+  }
+
 }
 
