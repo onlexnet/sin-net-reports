500cb5a0cffafdd67e83a3837120301ff6ee08eb
Slawomir Siudek <siudeks@gmail.com>
2021-06-24 08:41:31 +0200

    Add green test

--- uservice-reports/main/src/main/scala/sinnet/Mapper.scala
+++ uservice-reports/main/src/main/scala/sinnet/Mapper.scala
@@ -5,16 +5,17 @@ import java.time.LocalDate
 import scala.collection.JavaConverters._
 import scala.util.Try
 
+/** Converts ReportRequest (DTO) to local model. */
 object Mapper {
-  private def def4(x: sinnet.reports.Date): Option[LocalDate] = 
+  private implicit def toLocalDate(x: sinnet.reports.Date): Option[LocalDate] = 
     Try(LocalDate.of(x.getYear(), x.getMonth(), x.getDayOfTheMonth())).toOption
-  private implicit def def3(x: sinnet.reports.ActivityDetails) =
-    ActivityDetails(x.getDescription(), x.getWho(), def4(x.getWhen()), new Minutes(x.getHowLongInMins()), new Kilometers(x.getHowFarInKms()))
-  private implicit def def2(x: sinnet.reports.CustomerDetails) =
+  private implicit def toActivityDetails(x: sinnet.reports.ActivityDetails) =
+    ActivityDetails(x.getDescription(), x.getWho(), x.getWhen(), new Minutes(x.getHowLongInMins()), new Kilometers(x.getHowFarInKms()))
+  private implicit def toCustomerDetails(x: sinnet.reports.CustomerDetails) =
     CustomerDetails(x.getCustomerName(), x.getCustomerCity(), x.getCustomerAddress())
 
     
   implicit def apply(dto: ReportRequestDTO): ReportRequest = {
-    ReportRequest(dto.getCustomer(), dto.getDetailsList().asScala.map(def3 _))
+    ReportRequest(dto.getCustomer(), dto.getDetailsList().asScala.map(toActivityDetails _))
   }
 }
--- uservice-reports/main/src/test/scala/sinnet/MapperTests.scala
+++ uservice-reports/main/src/test/scala/sinnet/MapperTests.scala
@@ -2,12 +2,53 @@ package sinnet
 
 import org.junit.jupiter.api.Test
 import org.assertj.core.api.Assertions
-import sinnet.reports.{ReportRequest => ReportRequestDTO, ActivityDetails => ActivityDetailsDTO}
+import sinnet.reports.{ReportRequest => ReportRequestDTO,
+  ActivityDetails => ActivityDetailsDTO,
+  CustomerDetails => CustomerDetailsDTO,
+  Date}
+import java.time.LocalDate
 
 class MapperTests {
-  
+
   @Test
   def shouldConvertWhenEmptyActivityIsProvided(): Unit = {
-    var minModel = Mapper(ReportRequestDTO.newBuilder().addDetails(ActivityDetailsDTO.newBuilder().build()).build())
+    val minModel = Mapper(ReportRequestDTO.newBuilder().addDetails(ActivityDetailsDTO.newBuilder().build()).build())
+    // no exceptions mean success
+  }
+
+  @Test
+  def shouldConverSomeFullModel(): Unit = {
+    val dto = ReportRequestDTO
+      .newBuilder()
+      .setCustomer(
+        CustomerDetailsDTO.newBuilder()
+          .setCustomerAddress("my customer address")
+          .setCustomerCity("my customer city")
+          .setCustomerId("my customer id")
+          .setCustomerName("my customer name")
+          .build())
+      .addDetails(ActivityDetailsDTO.newBuilder()
+          .setDescription("activity 1 description")
+          .setHowFarInKms(11)
+          .setHowLongInMins(12)
+          .setWho("operator 1")
+          .setWhen(Date.newBuilder().setYear(2001).setMonth(1).setDayOfTheMonth(12).build()))
+      .addDetails(ActivityDetailsDTO.newBuilder()
+          .setDescription("activity 2 description")
+          .setHowFarInKms(21)
+          .setHowLongInMins(22)
+          .setWho("operator 2")
+          .setWhen(Date.newBuilder().setYear(2002).setMonth(2).setDayOfTheMonth(22).build()))
+      .build()
+
+    val expected = ReportRequest(
+      CustomerDetails("my customer name", "my customer city", "my customer address"),
+      Seq(
+        ActivityDetails("activity 1 description", "operator 1", Some(LocalDate.of(2001,1,12)), Minutes(12), Kilometers(11)),
+        ActivityDetails("activity 2 description", "operator 2", Some(LocalDate.of(2002,2,22)), Minutes(22), Kilometers(21))
+      )
+    )
+    val actual = Mapper(dto)
+    Assertions.assertThat(actual).isEqualTo(expected)
   }
 }
