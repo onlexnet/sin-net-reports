2e974d7fd7a828d284eaebf8bd5c2187d671bf2c
Slawomir Siudek <siudeks@gmail.com>
2021-06-27 21:59:59 +0200

    Formatting

--- webapi/main/domain-core/src/main/java/sinnet/read/ActionProjectionProvider.java
+++ webapi/main/domain-core/src/main/java/sinnet/read/ActionProjectionProvider.java
@@ -21,54 +21,54 @@ import sinnet.models.EntityId;
 @Component
 public class ActionProjectionProvider implements ActionProjection.Provider, ActionProjection {
 
-    @Autowired
-    private PgPool pgClient;
+  @Autowired
+  private PgPool pgClient;
 
-    private static final String VIEW_SELECT = "SELECT a.project_id, a.entity_id, a.entity_version, "
+  private static final String VIEW_SELECT = "SELECT a.project_id, a.entity_id, a.entity_version, "
             + "a.serviceman_email, a.distance, a.duration, a.date, a.description, a.serviceman_name, "
             + "a.customer_id, "
             + "c.customer_name, c.customer_city_name, c.customer_address "
             + "FROM actions a LEFT JOIN customers c on a.customer_id = c.entity_id ";
 
-    @Override
-    public Future<Array<ListItem>> find(UUID projectId, LocalDate from, LocalDate to) {
-        var promise = Promise.<Array<ListItem>>promise();
-        var findQuery = this.pgClient
-                .preparedQuery(VIEW_SELECT + " WHERE a.project_id=$1 AND a.date >= $2 AND a.date <= $3");
-        findQuery.execute(Tuple.of(projectId, from, to), ar -> {
-            if (ar.succeeded()) {
-                var rows = ar.result();
-                var value = Array.ofAll(rows).map(this::map);
-                promise.complete(value);
-            } else {
-                promise.fail(ar.cause());
-            }
-        });
-        return promise.future();
-    }
+  @Override
+  public Future<Array<ListItem>> find(UUID projectId, LocalDate from, LocalDate to) {
+    var promise = Promise.<Array<ListItem>>promise();
+    var findQuery = this.pgClient
+            .preparedQuery(VIEW_SELECT + " WHERE a.project_id=$1 AND a.date >= $2 AND a.date <= $3");
+    findQuery.execute(Tuple.of(projectId, from, to), ar -> {
+      if (ar.succeeded()) {
+        var rows = ar.result();
+        var value = Array.ofAll(rows).map(this::map);
+        promise.complete(value);
+      } else {
+        promise.fail(ar.cause());
+      }
+    });
+    return promise.future();
+  }
 
-    ListItem map(Row row) {
-        var projectId = row.getUUID("project_id");
-        var entityId = row.getUUID("entity_id");
-        var entityVersion = row.getInteger("entity_version");
-        var eid = EntityId.of(projectId, entityId, entityVersion);
-        var value = ActionValue.builder()
-            .who(Email.of(row.getString("serviceman_email")))
-            .howFar(Distance.of(row.getInteger("distance")))
-            .howLong(ActionDuration.of(row.getInteger("duration")))
-            .whom(row.getUUID("customer_id"))
-            .what(row.getString("description"))
-            .when(row.getLocalDate("date"))
-            .build();
-        var customerName = row.getString("customer_name");
-        var customerCity = row.getString("customer_city_name");
-        var customerAddress = row.getString("customer_address");
-        return ListItem.builder()
-            .eid(eid)
-            .value(value)
-            .customerName(customerName)
-            .customerCity(customerCity)
-            .customerAddress(customerAddress)
-            .build();
-    }
+  ListItem map(Row row) {
+    var projectId = row.getUUID("project_id");
+    var entityId = row.getUUID("entity_id");
+    var entityVersion = row.getInteger("entity_version");
+    var eid = EntityId.of(projectId, entityId, entityVersion);
+    var value = ActionValue.builder()
+        .who(Email.of(row.getString("serviceman_email")))
+        .howFar(Distance.of(row.getInteger("distance")))
+        .howLong(ActionDuration.of(row.getInteger("duration")))
+        .whom(row.getUUID("customer_id"))
+        .what(row.getString("description"))
+        .when(row.getLocalDate("date"))
+        .build();
+    var customerName = row.getString("customer_name");
+    var customerCity = row.getString("customer_city_name");
+    var customerAddress = row.getString("customer_address");
+    return ListItem.builder()
+        .eid(eid)
+        .value(value)
+        .customerName(customerName)
+        .customerCity(customerCity)
+        .customerAddress(customerAddress)
+        .build();
+  }
 }
