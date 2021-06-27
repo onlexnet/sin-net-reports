f98669a44af4259c7c7fa0f9de5ab6d9fa04acb5
Slawomir Siudek <siudeks@gmail.com>
2021-06-27 21:14:30 +0200

    Code refactor

--- webapi/main/domain-api/src/main/java/sinnet/models/ActionValue.java
+++ webapi/main/domain-api/src/main/java/sinnet/models/ActionValue.java
@@ -13,24 +13,24 @@ import lombok.extern.jackson.Jacksonized;
 @Builder(toBuilder = true)
 public class ActionValue implements EntityValue<ActionValue> {
 
-    /** Serviceman who did the service. */
-    @Builder.Default
-    private Email who = Email.empty();
+  /** Serviceman who did the service. */
+  @Builder.Default
+  private Email who = Email.empty();
 
-    /** Date when the service has been provided. */
-    private LocalDate when;
+  /** Date when the service has been provided. */
+  private LocalDate when;
 
-    /** Id of the Customer whom the service has been provided. */
-    private UUID whom;
+  /** Id of the Customer whom the service has been provided. */
+  private UUID whom;
 
-    /** Descriptive info what actually has been provided as the service. */
-    private String what;
+  /** Descriptive info what actually has been provided as the service. */
+  private String what;
 
-    /** How much time take overall activity related to provide the service. */
-    @Builder.Default
-    private ActionDuration howLong = ActionDuration.empty();
+  /** How much time take overall activity related to provide the service. */
+  @Builder.Default
+  private ActionDuration howLong = ActionDuration.empty();
 
-    /** How far is distance to the place where service has been provided. */
-    @Builder.Default
-    private Distance howFar = Distance.empty();
+  /** How far is distance to the place where service has been provided. */
+  @Builder.Default
+  private Distance howFar = Distance.empty();
 }
--- webapi/main/domain-api/src/main/java/sinnet/models/Distance.java
+++ webapi/main/domain-api/src/main/java/sinnet/models/Distance.java
@@ -7,29 +7,29 @@ import lombok.Getter;
 @EqualsAndHashCode
 public final class Distance {
 
-    private static Distance empty = new Distance(0);
+  private static Distance empty = new Distance(0);
 
-    @Getter
-    /**
-     * Value of the distance.
-     * Invariant: value >=0.
-     */
-    private final int value;
+  @Getter
+  /**
+   * Value of the distance.
+   * Invariant: value >=0.
+   */
+  private final int value;
 
-    private Distance(int value) {
-        this.value = Math.max(value, 0);
-    }
+  private Distance(int value) {
+    this.value = Math.max(value, 0);
+  }
 
-    public static Distance of(int value) {
-        return new Distance(value);
-    }
+  public static Distance of(int value) {
+    return new Distance(value);
+  }
 
-    public static Distance empty() {
-        return empty;
-    }
+  public static Distance empty() {
+    return empty;
+  }
 
-    @Override
-    public String toString() {
-        return Integer.toString(value);
-    }
+  @Override
+  public String toString() {
+    return Integer.toString(value);
+  }
 }
