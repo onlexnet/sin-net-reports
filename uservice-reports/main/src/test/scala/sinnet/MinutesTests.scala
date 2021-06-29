06656b285ff2f8d6f99dfa2debe7bf90e8cd6639
Slawomir Siudek <siudeks@gmail.com>
2021-06-29 10:38:46 +0200

    More tests

---
+++ uservice-reports/main/src/test/scala/sinnet/MinutesTests.scala
@@ -0,0 +1,13 @@
+package sinnet
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class MinutesTests {
+
+    @Test
+    def shouldFormatString: Unit = {
+      Assertions.assertThat(Minutes(15).toString()).isEqualTo("0:15");
+      Assertions.assertThat(Minutes(7381).toString()).isEqualTo("123:01");
+    }
+}
