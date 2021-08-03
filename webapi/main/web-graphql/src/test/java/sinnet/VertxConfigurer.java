4526d561706f929229b542dfa61caf805e0ccaee
Slawomir Siudek <siudeks@gmail.com>
2021-08-03 11:26:54 +0200

    Playing with OpenTelemetry

--- webapi/main/web-graphql/src/test/java/sinnet/VertxConfigurer.java
+++ webapi/main/web-graphql/src/test/java/sinnet/VertxConfigurer.java
@@ -4,28 +4,35 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
 import io.vertx.core.eventbus.EventBus;
+import io.vertx.tracing.opentelemetry.OpenTelemetryOptions;
 
 /** Single configuration class for Vertx related components. */
 @TestConfiguration
 public class VertxConfigurer {
 
-    /**
-     * Fixme.
-     * @return fixme
-     */
-    @Bean
-    public Vertx vertx() {
-        return Vertx.vertx();
-    }
+  /**
+   * Fixme.
+   *
+   * @return shared vertx instance use across layers
+   */
+  @Bean
+  public Vertx vertx() {
+    return Vertx.vertx(
+      new VertxOptions()
+      .setTracingOptions(
+        new OpenTelemetryOptions())
+    );
+  }
 
-    /** Fixme.
-     *
-     * @param vertx fixme
-     * @return fixme
-     */
-    @Bean
-    public EventBus vertxEventBus(Vertx vertx) {
-        return vertx.eventBus();
-    }
+  /** Fixme.
+   *
+   * @param vertx fixme
+   * @return fixme
+   */
+  @Bean
+  public EventBus vertxEventBus(Vertx vertx) {
+    return vertx.eventBus();
+  }
 }
