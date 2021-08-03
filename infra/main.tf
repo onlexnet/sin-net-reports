71c7d719fc8186ed980b9418dddf8d8231bcb843
Slawomir Siudek <siudeks@gmail.com>
2021-08-03 16:11:18 +0200

    Remove commented part

--- infra/main.tf
+++ infra/main.tf
@@ -41,26 +41,3 @@ module "database" {
   db_user_name = var.psql_infrauser_name
   db_user_password = var.psql_infrauser_password
 }
-
-# data "azurerm_client_config" "current" {
-# #   client_id     = data.azurerm_client_config.current.client_id
-# #   client_secret = data.azurerm_client_config.current.
-# #   tenant_id     = data.azurerm_client_config.current.tenant_id
-# }
-
-
-
-# data "azurerm_container_registry" "example" {
-#     provider = azurerm.sinnet-production
-#     name                = "sinnet"
-#     resource_group_name = "sinnet-default-manual"
-# }
-
-# resource "azurerm_storage_account" "default" {
-#     resource_group_name = azurerm_resource_group.default.name
-#     name = "${var.resource_group_name}${var.environment}storage"
-#     location = var.location
-#     account_tier = "Standard"
-#     account_replication_type = "LRS"
-# }
-
