ea551f9d778e8654b1e618f4f3ee5bcdfa7d819f
Slawomir Siudek <siudeks@gmail.com>
2021-09-14 22:28:01 +0200

    Debug info about dev mode

--- webapp/main/src/debug/Debug.tsx
+++ webapp/main/src/debug/Debug.tsx
@@ -19,6 +19,7 @@ export const Debug: React.FC<Props> = () => {
       <p>data: {String(data)}</p>
       <p>loading: {String(loading)}</p>
       <p>error: {String(error)}</p>
+      <p><small>You are running this application in <b>{process.env.NODE_ENV}</b> mode.</small></p>
 
       {isAuthenticated && <DefaultButton onClick={() => a.instance.logout()}>Logout</DefaultButton>}
       <p>DEBUG</p>
