f081156dabc2edf023f1379d43b992acca135d0a
Slawomir Siudek <siudeks@gmail.com>
2021-09-29 23:08:02 +0200

    New uservice for graphql initial commit

---
+++ uservice-webapi/sinnet/src/main/java/sinnet/ProjectResource.java
@@ -0,0 +1,26 @@
+package sinnet;
+
+import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Id;
+import org.eclipse.microprofile.graphql.NonNull;
+import org.eclipse.microprofile.graphql.Query;
+
+import lombok.Data;
+
+@GraphQLApi
+public class ProjectResource {
+
+  @Query
+  public @NonNull ProjectEntity[] availableProjects() {
+    return new ProjectEntity[0];
+  }
+}
+
+@Data
+class ProjectEntity {
+  private @Id @NonNull String id;
+  private @NonNull String name;
+}
+
+
+
