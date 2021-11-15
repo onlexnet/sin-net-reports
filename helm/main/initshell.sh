2fcbbb8d78e9d45292238d4dea64081f1de76858
Slawomir Siudek <slawomir@siudek.net>
2021-11-15 22:30:17 +0100

    Add description to script

--- helm/main/initshell.sh
+++ helm/main/initshell.sh
@@ -1,6 +1,9 @@
 # usage: 
+# to test locally script on namespace dev01 use
 # . initshell.sh dev01
-# . initshell.sh prd01
+# so later on you may use aliases
+# k == kubectl --namespace onlex-sinnet-dev01
+# h == helm --namespace onlex-sinnet-dev01
 namespace="onlex-sinnet-$1"
 alias kc="kubectl"
 alias k="kc --namespace $namespace"
