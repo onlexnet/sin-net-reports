11acf522be0ec4ed924e812ca3c517b15a2e251b
Slawomir Siudek <slawomir@siudek.net>
2021-11-16 20:35:38 +0100

    Remove unused variable

--- infra/environments/all-prod/variables.tf
+++ infra/environments/all-prod/variables.tf
@@ -1,7 +1,3 @@
 # shared names reused across environments
 variable "sinnet_k8s_host" {}
 variable "sinnet_k8s_token" {}
-
-# customized name so that multiple subscription ids may be defined in one file
-variable "sinnet_prod_subscription_id" {}
-
