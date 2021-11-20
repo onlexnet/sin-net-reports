5e3d115787a6fed80caafc423394e45903218d58
Slawomir Siudek <slawomir@siudek.net>
2021-11-20 22:44:40 +0100

    UTC -> Local on UI

--- webapp/main/src/app/customer/SecretsTimestamp.ts
+++ webapp/main/src/app/customer/SecretsTimestamp.ts
@@ -7,9 +7,9 @@ export interface SecretsTimestamp {
 /** Build from UTD datetime, forematted to dd/MM/yyyy hh:mm */
 export class SecretsTimestamp {
     public static of(utc: string) : SecretsTimestamp {
-        var date = new Date(utc)
+        var stillUtc = moment.utc(utc).toDate()
         // more https://momentjscom.readthedocs.io/en/latest/moment/04-displaying/01-format/
-        var local = moment(date).local().format('DD/MM/yyyy HH:mm');
+        var local = moment(stillUtc).format("DD/MM/yyyy HH:mm");
         return { value: local};
     }
 }
