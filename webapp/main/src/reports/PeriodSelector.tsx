62dd5eedd82b5f54d03eb7987f3149b67fda0737
Sławomir Siudek <slawomir@siudek.net>
2022-01-11 23:17:48 +0100

    Siudeks/issue47 (#75)
    * Upgrade to 2.3.0
    
    * Extract common part of proto
    
    * Fixed compilation
    
    * Adjust new api for raport2
    
    * WIP
    
    * Compilable
    
    * Red test
    
    * Red test
    
    * Green test
    
    * Can produce Raport2
    
    * Refactor
    
    * Refactor
    
    * Refactor
    
    * Refactor
    
    * Remove unused code
    
    * Rename folder
    
    * Bump Quarkus to 2.4.2
    
    * Fix code duplication
    
    * Skończony raport
    
    * Build fix
    
    * Add new menu position for new raport
    
    * Cleanup warnings
    
    * Rename to make reports orderable
    
    * Add storage for short living reports
    
    * Storage rules for Reports
    
    * ConfigMap about Storage
    
    * Add Azure SDK
    
    * grpc migration
    
    * Reports view is routable
    
    * Doc
    
    * Nowy raport
    
    * Nowy raport
    
    * Raport zestawienie godzin pracownicy
    
    * Raport zestawienie godzin pracownicy
    
    * bump .version
    
    * Raport zestawienie godzin pracownicy
    
    * Finally working report

---
+++ webapp/main/src/reports/PeriodSelector.tsx
@@ -0,0 +1,48 @@
+import { Label, Stack, TextField } from "@fluentui/react";
+import React, { useState } from "react";
+
+interface PeriodSelectorProps {
+    suffix: string,
+    year?: number,
+    month?: number,
+    onYearChanged: (value?: number) => void,
+    onMonthChanged: (value?: number) => void,
+}
+
+
+export const PeriodSelector: React.FC<PeriodSelectorProps> = props => {
+
+    const yearRe = /^(19|20)\d{2}$/;
+    const monthRe = /^(0?[1-9]|1[012])$/;
+  
+    const onChangeYear = (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
+      setYearText(newValue);
+  
+      const newValueAsString = newValue ?? "";
+      const newValueAsInt = yearRe.test(newValueAsString) ? parseInt(newValueAsString) : undefined;
+      if (newValueAsInt === props.year) return;
+      props.onYearChanged(newValueAsInt);
+    }
+  
+    const onChangeMonth = (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
+      setMonthText(newValue);
+  
+      const newValueAsString = newValue ?? "";
+      const newValueAsInt = monthRe.test(newValueAsString) ? parseInt(newValueAsString) : undefined;
+      if (newValueAsInt === props.month) return;
+      props.onMonthChanged(newValueAsInt);
+    }
+  
+    const [yearText, setYearText] = useState<string | undefined>((props.year ?? '').toString());
+    const [monthText, setMonthText] = useState<string | undefined>((props.month ?? '').toString());
+  
+    return (<Stack horizontal>
+        <Stack.Item>
+            <TextField label={`Rok ${props.suffix}`} value={yearText} onChange={onChangeYear} />
+        </Stack.Item>
+        <Stack.Item>
+            <TextField label={`Miesiąc ${props.suffix}`} value={monthText} onChange={onChangeMonth} />
+        </Stack.Item>
+    </Stack>);
+
+}
--- webapp/main/src/reports/ReportsView.tsx
+++ webapp/main/src/reports/ReportsView.tsx
@@ -1,7 +1,8 @@
-import { Label, Link, Stack } from "@fluentui/react";
-import React from "react";
+import { Link, Stack } from "@fluentui/react";
+import React, { useState } from "react";
 import { addressProvider } from "../addressProvider";
 import { LocalDate } from "../store/viewcontext/TimePeriod";
+import { PeriodSelector } from "./PeriodSelector";
 
 const stackTokens = { childrenGap: 8 }
 
@@ -13,27 +14,36 @@ interface ReportsViewProps {
 
 export const ReportsView: React.FC<ReportsViewProps> = props => {
   const { projectId, from } = props;
-  const { year, month } = from;
   const openInNewTab = (url: string) => {
     window.open(url, '_blank');
   }
+
+  const [fromYear, setFromYear] = useState<number | undefined>(from.year);
+  const [fromMonth, setFromMonth] = useState<number | undefined>(from.month);
+
+  const [toYear, setToYear] = useState<number | undefined>(from.year);
+  const [toMonth, setToMonth] = useState<number | undefined>(from.month);
+
   return (
     <Stack verticalFill style={{ padding: 10 }}>
       <Stack.Item>
         <Stack tokens={stackTokens}>
           <h1>Raporty</h1>
-          <Label>Rok: {year}, miesiąc: {month}</Label>
-          <Link onClick={() => {
-            openInNewTab(addressProvider().host + `/api/raporty/klienci/${projectId}/${year}/${month}`);
-          }}>
-            Raport miesięczny
+          <PeriodSelector suffix="OD:" year={fromYear} month={fromMonth} onYearChanged={setFromYear} onMonthChanged={setFromMonth}/>
+          <PeriodSelector suffix="DO:" year={toYear} month={toMonth} onYearChanged={setToYear} onMonthChanged={setToMonth}/>
+
+          <Link
+            disabled={fromYear == null || fromMonth == null}
+            onClick={() => { openInNewTab(addressProvider().host + `/api/raporty/klienci/${projectId}/${fromYear}/${fromMonth}`); }}
+          >
+            Raport miesięczny (na datę OD)
           </Link>
 
 
           <Link onClick={() => {
-            openInNewTab(addressProvider().host + `/api/raporty/2/${projectId}/${year}/${month}`);
+            openInNewTab(addressProvider().host + `/api/raporty/2/${projectId}?yearFrom=${fromYear}&monthFrom=${fromMonth}&yearTo=${toYear}&monthTo=${toMonth}`);
           }}>
-            Zestawienie sumaryczne
+            Zestawienie sumaryczne (na okres OD - DO)
           </Link>
 
         </Stack>
