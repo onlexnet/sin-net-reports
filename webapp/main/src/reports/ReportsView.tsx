7a63cae6dc372cafd5e72ff9f0dc7234a428e4cb
Slawomir Siudek <slawomir@siudek.net>
2022-01-11 22:51:34 +0100

    Raport zestawienie godzin pracownicy

--- webapp/main/src/reports/ReportsView.tsx
+++ webapp/main/src/reports/ReportsView.tsx
@@ -1,8 +1,8 @@
-import { Label, Link, Stack, TextField } from "@fluentui/react";
-import React, { useCallback, useState } from "react";
-import { isNumericLiteral } from "typescript";
+import { Link, Stack } from "@fluentui/react";
+import React, { useState } from "react";
 import { addressProvider } from "../addressProvider";
 import { LocalDate } from "../store/viewcontext/TimePeriod";
+import { PeriodSelector } from "./PeriodSelector";
 
 const stackTokens = { childrenGap: 8 }
 
@@ -18,55 +18,32 @@ export const ReportsView: React.FC<ReportsViewProps> = props => {
     window.open(url, '_blank');
   }
 
-  const [fromYearText, setFromYearText] = useState<string | undefined>(from.year.toString());
-  const [fromMonthText, setFromMonthText] = useState<string | undefined>(from.month.toString());
-  const [fromYear, setFromYear] = useState<number | null>(from.year);
-  const [FromMonth, setFromMonth] = useState<number | null>(from.month);
+  const [fromYear, setFromYear] = useState<number | undefined>(from.year);
+  const [fromMonth, setFromMonth] = useState<number | undefined>(from.month);
 
-  const yearRe = /^(19|20)\d{2}$/;
-  const monthRe = /^(0?[1-9]|1[012])$/;
-
-  const onChangeYear = (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
-    setFromYearText(newValue);
-
-    const newValueAsString = newValue ?? "";
-    const newValueAsInt = yearRe.test(newValueAsString) ? parseInt(newValueAsString) : null;
-    setFromYear(newValueAsInt);
-  }
-
-  const onChangeMonth = (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
-    setFromMonthText(newValue);
-
-    const newValueAsString = newValue ?? "";
-    const newValueAsInt = monthRe.test(newValueAsString) ? parseInt(newValueAsString) : null;
-    setFromMonth(newValueAsInt);
-  }
+  const [toYear, setToYear] = useState<number | undefined>(from.year);
+  const [toMonth, setToMonth] = useState<number | undefined>(from.month);
 
   return (
     <Stack verticalFill style={{ padding: 10 }}>
       <Stack.Item>
         <Stack tokens={stackTokens}>
           <h1>Raporty</h1>
-          <Stack horizontal>
-            <Stack.Item>
-              <TextField label="Rok" value={fromYearText} onChange={onChangeYear} />
-            </Stack.Item>
-            <Stack.Item>
-              <TextField label="Miesiąc" value={fromMonthText} onChange={onChangeMonth} />
-            </Stack.Item>
-          </Stack>
+          <PeriodSelector suffix="OD:" year={fromYear} month={fromMonth} onYearChanged={setFromYear} onMonthChanged={setFromMonth}/>
+          <PeriodSelector suffix="DO:" year={toYear} month={toMonth} onYearChanged={setToYear} onMonthChanged={setToMonth}/>
+
           <Link
-            disabled={fromYear == null || FromMonth == null}
-            onClick={() => { openInNewTab(addressProvider().host + `/api/raporty/klienci/${projectId}/${fromYearText}/${fromMonthText}`); }}
+            disabled={fromYear == null || fromMonth == null}
+            onClick={() => { openInNewTab(addressProvider().host + `/api/raporty/klienci/${projectId}/${fromYear}/${fromMonth}`); }}
           >
-            Raport miesięczny
+            Raport miesięczny (na datę OD)
           </Link>
 
 
           <Link onClick={() => {
-            openInNewTab(addressProvider().host + `/api/raporty/2/${projectId}/${fromYearText}/${fromMonthText}`);
+            openInNewTab(addressProvider().host + `/api/raporty/2/${projectId}/${fromYear}/${fromMonth}`);
           }}>
-            Zestawienie sumaryczne
+            Zestawienie sumaryczne (na okres OD - DO)
           </Link>
 
         </Stack>
