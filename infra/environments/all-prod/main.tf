7e188128751df7973072873694669db726c775b8
Sławomir Siudek <slawomir@siudek.net>
2022-01-13 22:48:27 +0100

    Siudeks/issue48 (#76)
    * Terraform changes
    
    * Chore: terraform
    
    * Working report

--- infra/environments/all-prod/main.tf
+++ infra/environments/all-prod/main.tf
@@ -4,6 +4,13 @@ resource "kubernetes_namespace" "default" {
   }
 }
 
+module "kubernetes" {
+  source         = "./module_kubernetes"
+  namespace_name = local.namespace_name
+}
+
 module "azdo" {
-  source = "./module_azdo"
+  source                  = "./module_azdo"
+  service_account_token   = module.kubernetes.devops-cluster-svc-token
+  service_account_ca_cert = module.kubernetes.ca-cert
 }
--- infra/environments/all-prod/module_kubernetes/azure-devops-cluster-svc.tf
+++ infra/environments/all-prod/module_kubernetes/azure-devops-cluster-svc.tf
@@ -1,17 +1,17 @@
 # Why we need service account?
 # - In Azure DevOps, to connect Environment to k8s namespace we have to point a service account
 # more: https://raaviblog.com/how-to-create-azure-service-connection-to-connect-to-kubernetes/
-resource "kubernetes_service_account" "azure-devops-cluster-svc" {
+resource "kubernetes_service_account" "devops-cluster-svc" {
   metadata {
-    name      = "azure-devops-cluster-svc"
-    namespace = local.namespace_name
+    name      = "devops-cluster-svc"
+    namespace = var.namespace_name
   }
 }
 
 # Minimal role required to connect namespace to Azure DevOps
-resource "kubernetes_cluster_role" "azure-devops-cluster-svc" {
+resource "kubernetes_cluster_role" "devops-cluster-svc" {
   metadata {
-    name = "azure-devops-cluster-svc"
+    name = "devops-cluster-svc"
   }
 
   # required to connect namespace to ADO
@@ -31,21 +31,27 @@ resource "kubernetes_cluster_role" "azure-devops-cluster-svc" {
 
 }
 
-resource "kubernetes_cluster_role_binding" "azure-devops-cluster-svc-binding" {
+resource "kubernetes_cluster_role_binding" "devops-cluster-svc-binding" {
   metadata {
-    name = "azure-devops-cluster-svc-binding"
+    name = "devops-cluster-svc-binding"
   }
 
   role_ref {
     api_group = "rbac.authorization.k8s.io"
     kind      = "ClusterRole"
-    name      = kubernetes_cluster_role.azure-devops-cluster-svc.metadata[0].name
+    name      = kubernetes_cluster_role.devops-cluster-svc.metadata[0].name
   }
 
   subject {
     kind      = "ServiceAccount"
-    name      = kubernetes_service_account.azure-devops-cluster-svc.metadata[0].name
-    namespace = local.namespace_name
+    name      = kubernetes_service_account.devops-cluster-svc.metadata[0].name
+    namespace = var.namespace_name
   }
 }
 
+data "kubernetes_secret" "devops-cluster-svc" {
+  metadata {
+    name = kubernetes_service_account.devops-cluster-svc.default_secret_name
+    namespace = var.namespace_name
+  }
+}
