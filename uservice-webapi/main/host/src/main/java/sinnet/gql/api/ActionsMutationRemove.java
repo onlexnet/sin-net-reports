5ed63cc3677dc09083b5ada5e9b6b3c867e47ec6
Slawomir Siudek <slawomir@siudek.net>
2022-02-26 12:51:38 +0100

    WIP

---
+++ uservice-webapi/main/host/src/main/java/sinnet/gql/api/ActionsMutationRemove.java
@@ -0,0 +1,37 @@
+package sinnet.gql.api;
+
+import org.eclipse.microprofile.graphql.GraphQLApi;
+import org.eclipse.microprofile.graphql.Id;
+import org.eclipse.microprofile.graphql.NonNull;
+import org.eclipse.microprofile.graphql.Source;
+
+import io.quarkus.grpc.GrpcClient;
+import io.smallrye.mutiny.Uni;
+import lombok.extern.slf4j.Slf4j;
+import sinnet.ActionsMutation;
+import sinnet.gql.Transform;
+import sinnet.grpc.common.EntityId;
+import sinnet.grpc.timeentries.RemoveCommand;
+import sinnet.grpc.timeentries.RemoveResult;
+import sinnet.grpc.timeentries.TimeEntries;
+
+@GraphQLApi
+@Slf4j
+public class ActionsMutationRemove {
+
+  @GrpcClient("activities")
+  TimeEntries service;
+
+  public Uni<@NonNull Boolean> remove(@Source ActionsMutation self, @NonNull @Id String entityId, int entityVersion) {
+    var cmd = RemoveCommand.newBuilder()
+        .setEntityId(EntityId.newBuilder()
+          .setProjectId(self.getProjectId())
+          .setEntityId(entityId)
+          .setEntityVersion(entityVersion)
+          .build())
+        .build();
+    return service.remove(cmd)
+      .onItemOrFailure()
+      .transform(Transform.logged(log, RemoveResult::getResult));
+  }
+}
