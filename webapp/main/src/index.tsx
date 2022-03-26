c70cfa037071771b8cb2de1e72357b5bba696844
Slawomir Siudek <slawomir@siudek.net>
2022-03-26 21:27:26 +0100

    bump dependencies

--- webapp/main/src/index.tsx
+++ webapp/main/src/index.tsx
@@ -12,7 +12,17 @@ import App from "./app/App";
 import { AppInsightsContext } from "@microsoft/applicationinsights-react-js";
 import { reactPlugin } from "./app/AppInsight";
 
+import {
+  AzureThemeLight,
+  AzureThemeDark,
+  AzureThemeHighContrastLight,
+  AzureThemeHighContrastDark,
+} from '@fluentui/azure-themes';
+
 initializeIcons();
+
+// https://github.com/microsoft/fluentui/tree/master/packages/azure-themes
+const theme = AzureThemeLight; // or alternatively AzureThemeDark
 
 ReactDOM.render(
   <ThemeProvider>
