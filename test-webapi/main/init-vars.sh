fd62e443fc312176b5d9efe1098d09f9f2f5bfba
Slawomir Siudek <slawomir@siudek.net>
2022-04-15 09:08:59 +0200

    Read user credentials

--- test-webapi/main/init-vars.sh
+++ test-webapi/main/init-vars.sh
@@ -1,3 +1,3 @@
 az account set --subscription onlexnet-sinnet-app-stg01
-OPERATOR1_NAME=$(az keyvault secret show --name "test-operator-1-email" --vault-name "sinnet-stg01" --query "value")
-OPERATOR1_PASSWORD=$(az keyvault secret show --name "test-operator-1-password" --vault-name "sinnet-stg01" --query "value")
+export OPERATOR1_NAME=$(az keyvault secret show --name "test-operator-1-email" --vault-name "sinnet-stg01" --query "value")
+export OPERATOR1_PASSWORD=$(az keyvault secret show --name "test-operator-1-password" --vault-name "sinnet-stg01" --query "value")
--- test-webapi/main/src/test/java/net/onlex/UserLogin.java
+++ test-webapi/main/src/test/java/net/onlex/UserLogin.java
@@ -3,6 +3,7 @@ package net.onlex;
 import javax.inject.Inject;
 
 import org.assertj.core.api.Assertions;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -14,9 +15,16 @@ public class UserLogin
   @Inject
   MyService myService;
 
+  @ConfigProperty(name = "OPERATOR1_NAME")
+  String operator1Name;
+  
+  @ConfigProperty(name = "OPERATOR1_PASSWORD")
+  String operator1Password;
+
   @When("I login using proper credentials")
   public void i_login_using_proper_credentials() {
     Assertions.assertThat(myService).isNotNull();
+    Assertions.assertThat(operator1Name).isNotNull();
   }
 
   @Then("I may get list of my projects")
