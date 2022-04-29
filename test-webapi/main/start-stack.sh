74fd144c6e89aa8a20ec777698ad2aa8d362fcff
Slawomir Siudek <slawomir@siudek.net>
2022-04-29 12:09:32 +0200

    Green gherkin tests with local stack

--- test-webapi/main/start-stack.sh
+++ test-webapi/main/start-stack.sh
@@ -1,1 +1,7 @@
-docker-compose -f ../../service-postgres/dc.yaml up
+cd ../../uservice-webapi/main && dapr --components-path .components run -- mvn quarkus:dev -pl host & 
+P1=$!
+
+cd ../../webapi/main && APP_DB_HOST=localhost APP_DB_NAME=postgres SPRING_PROFILES_ACTIVE=dev mvn spring-boot:run -pl host
+P2=$!
+
+wait $P1 $P2
