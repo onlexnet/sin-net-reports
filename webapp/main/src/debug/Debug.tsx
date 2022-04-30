388247d7190c79a7651cea5f6975055a50b5c867
Slawomir Siudek <slawomir@siudek.net>
2022-04-30 22:30:52 +0200

    Add version to DEBUG view

--- webapp/main/src/debug/Debug.tsx
+++ webapp/main/src/debug/Debug.tsx
@@ -1,19 +1,34 @@
 import { useIsAuthenticated, useMsal } from "@azure/msal-react";
-import { DefaultButton } from "@fluentui/react";
+import { DefaultButton, Stack } from "@fluentui/react";
 import React from "react";
+import packageJson from '../../package.json';
 
 
 interface Props {
 }
 
 export const Debug: React.FC<Props> = () => {
+  const stackTokens = { childrenGap: 8 }
+
   const a = useMsal();
   const isAuthenticated = useIsAuthenticated();
 
   return (
-    <>
-      <p><small>You are running this application in <b>{process.env.NODE_ENV}</b> mode.</small></p>
-      {isAuthenticated && <DefaultButton onClick={() => a.instance.logout()}>Logout</DefaultButton>}
-    </>
+    <Stack tokens={stackTokens}>
+      <Stack.Item>
+        <p><small>You are running this application in <b>{process.env.NODE_ENV}</b> mode.</small></p>
+      </Stack.Item>
+      <Stack.Item>
+        {isAuthenticated && <DefaultButton onClick={() => a.instance.logout()}>Logout</DefaultButton>}
+      </Stack.Item>
+      <Stack.Item>
+        <p>version: {packageJson.version}</p>
+      </Stack.Item>
+    </Stack >
   );
 };
+
+
+
+
+
