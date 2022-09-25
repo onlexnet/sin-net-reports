d300b347f2c0f6f875a8e4781cc51bbe4443a778
Sławomir Siudek <slawomir@siudek.net>
2022-09-25 23:44:32 +0200

    Red test

--- test-webapi/main/src/test/java/net/onlex/TimesheetsOperations.java
+++ test-webapi/main/src/test/java/net/onlex/TimesheetsOperations.java
@@ -21,25 +21,25 @@ public class TimesheetsOperations {
   @When("{userName} creates timeentry for {projectName}")
   public void operator1_creates_timeentry_for_project1(UserEmail operator, Project project) {
       // Write code here that turns the phrase above into concrete actions
-      throw new io.cucumber.java.PendingException();
+      throw new io.cucumber.java.PendingException("{userName} creates timeentry for {projectName}");
   }
 
   @Then("operation is rejected")
   public void operation_is_rejected() {
       // Write code here that turns the phrase above into concrete actions
-      throw new io.cucumber.java.PendingException();
+      throw new io.cucumber.java.PendingException("operation is rejected");
   }
 
-  @Then("number of timesheets in project {int} is zero")
-  public void number_of_timesheets_in_project_is_zero(Integer int1) {
+  @Then("number of timesheets in {projectName} is zero")
+  public void number_of_timesheets_in_project_is_zero(Project project) {
       // Write code here that turns the phrase above into concrete actions
-      throw new io.cucumber.java.PendingException();
+      throw new io.cucumber.java.PendingException("number of timesheets in project {projectName} is zero");
   }
 
   @When("User user1 assigns operator1 to project1")
   public void user_user1_assigns_operator1_to_project1() {
       // Write code here that turns the phrase above into concrete actions
-      throw new io.cucumber.java.PendingException();
+      throw new io.cucumber.java.PendingException("User user1 assigns operator1 to project1");
   }
 
 }
