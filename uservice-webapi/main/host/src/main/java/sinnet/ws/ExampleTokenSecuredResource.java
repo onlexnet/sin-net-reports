cf79d3377b14225752b41abf6ad7f7e6ddcceb6c
Sławomir Siudek <slawomir@siudek.net>
2022-11-04 20:07:20 +0100

    chore: checkstyle

--- uservice-webapi/main/host/src/main/java/sinnet/ws/ExampleTokenSecuredResource.java
+++ uservice-webapi/main/host/src/main/java/sinnet/ws/ExampleTokenSecuredResource.java
@@ -15,62 +15,63 @@ import org.eclipse.microprofile.jwt.JsonWebToken;
 import io.quarkus.security.Authenticated;
 import io.smallrye.jwt.auth.principal.DefaultJWTCallerPrincipal;
 
+/** TODO: delete. */
 @Path("/secured")
 public class ExampleTokenSecuredResource {
 
-    @Inject
-    JsonWebToken jwt;
+  @Inject
+  JsonWebToken jwt;
 
-    @GET()
-    @Path("permit-all")
-    @PermitAll
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello(@Context SecurityContext ctx) {
-        return getResponseString(ctx);
-    }
+  @GET()
+  @Path("permit-all")
+  @PermitAll
+  @Produces(MediaType.TEXT_PLAIN)
+  public String hello(@Context SecurityContext ctx) {
+    return getResponseString(ctx);
+  }
 
-    @GET()
-    @Path("debug")
-    @PermitAll
-    @Produces(MediaType.TEXT_PLAIN)
-    public String debug(@Context SecurityContext ctx) {
-        return Integer.toString(jwt.hashCode());
-    }
+  @GET
+  @Path("debug")
+  @PermitAll
+  @Produces(MediaType.TEXT_PLAIN)
+  public String debug(@Context SecurityContext ctx) {
+    return Integer.toString(jwt.hashCode());
+  }
 
-    @GET
-    @Path("roles-allowed") 
-    @RolesAllowed({ "User", "Admin" }) 
-    @Produces(MediaType.TEXT_PLAIN)
-    public String helloRolesAllowed(@Context SecurityContext ctx) {
-        return getResponseString(ctx) + ", birthdate: " + jwt.getClaim("birthdate").toString(); 
-    }
+  @GET
+  @Path("roles-allowed") 
+  @RolesAllowed({ "User", "Admin" }) 
+  @Produces(MediaType.TEXT_PLAIN)
+  public String helloRolesAllowed(@Context SecurityContext ctx) {
+    return getResponseString(ctx) + ", birthdate: " + jwt.getClaim("birthdate").toString(); 
+  }
 
-    @GET
-    @Path("authenticated")
-    @Authenticated
-    @Produces(MediaType.TEXT_PLAIN)
-    public String authentiocated(@Context SecurityContext ctx) {
-        var principal = (DefaultJWTCallerPrincipal) ctx.getUserPrincipal();
-        return principal.getClaimNames().toString();
-    }
+  @GET
+  @Path("authenticated")
+  @Authenticated
+  @Produces(MediaType.TEXT_PLAIN)
+  public String authentiocated(@Context SecurityContext ctx) {
+    var principal = (DefaultJWTCallerPrincipal) ctx.getUserPrincipal();
+    return principal.getClaimNames().toString();
+  }
 
-    private String getResponseString(SecurityContext ctx) {
-        String name;
-        if (ctx.getUserPrincipal() == null) {
-            name = "anonymous";
-        } else if (!ctx.getUserPrincipal().getName().equals(jwt.getName())) {
-            throw new IllegalStateException("Principal and JsonWebToken names do not match");
-        } else {
-            name = ctx.getUserPrincipal().getName();
-        }
-        return String.format("hello + %s,"
-                + " isHttps: %s,"
-                + " authScheme: %s,"
-                + " hasJWT: %s",
-                name, ctx.isSecure(), ctx.getAuthenticationScheme(), hasJwt());
+  private String getResponseString(SecurityContext ctx) {
+    String name;
+    if (ctx.getUserPrincipal() == null) {
+      name = "anonymous";
+    } else if (!ctx.getUserPrincipal().getName().equals(jwt.getName())) {
+      throw new IllegalStateException("Principal and JsonWebToken names do not match");
+    } else {
+      name = ctx.getUserPrincipal().getName();
     }
+    return String.format("hello + %s,"
+            + " isHttps: %s,"
+            + " authScheme: %s,"
+            + " hasJWT: %s",
+            name, ctx.isSecure(), ctx.getAuthenticationScheme(), hasJwt());
+  }
 
-    private boolean hasJwt() {
-        return jwt.getClaimNames() != null;
-    }
+  private boolean hasJwt() {
+    return jwt.getClaimNames() != null;
+  }
 }
