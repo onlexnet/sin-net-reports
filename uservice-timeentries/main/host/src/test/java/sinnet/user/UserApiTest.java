b5df69fc0b132f36c95192c75c43e7bcdf80dd8d
Sławomir Siudek <slawomir@siudek.net>
2022-11-26 10:31:09 +0100

    Green test

--- uservice-timeentries/main/host/src/test/java/sinnet/user/UserApiTest.java
+++ uservice-timeentries/main/host/src/test/java/sinnet/user/UserApiTest.java
@@ -1,21 +1,45 @@
 package sinnet.user;
 
+import java.util.UUID;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
+import io.grpc.ManagedChannelBuilder;
 import sinnet.db.PostgresDbExtension;
+import sinnet.grpc.common.UserToken;
+import sinnet.grpc.users.SearchReply;
+import sinnet.grpc.users.SearchRequest;
+import sinnet.grpc.users.UsersGrpc;
 
 @DisplayNameGeneration(DisplayNameGenerator.IndicativeSentences.class)
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
 @ExtendWith(PostgresDbExtension.class)
 class UserApiTest {
   
+  @Value("${grpc.server.port}")
+  private int grpcPort;
+
   @Test
-  void initializeContext() {
+  public void readEmptyUsers() {
+
+    var channel = ManagedChannelBuilder.forAddress("localhost", grpcPort)
+        .usePlaintext()
+        .build();
+    var token = UserToken.newBuilder().setProjectId(UUID.randomUUID().toString()).build();
+    var service = UsersGrpc.newBlockingStub(channel);
+    var request = SearchRequest.newBuilder().setUserToken(token) .build();
     
+    var actual = service.search(request);
+    
+    var expected = SearchReply.newBuilder().build();
+
+    Assertions.assertThat(actual).isEqualTo(expected);
   }
 }
