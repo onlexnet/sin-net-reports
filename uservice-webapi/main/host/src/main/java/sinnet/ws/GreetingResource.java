d1bc9d0aef949ec6cd7d1e5a12142ceaabcf005e
Sławomir Siudek <slawomir@siudek.net>
2022-12-11 01:20:24 +0100

    Fix tests

--- uservice-webapi/main/host/src/main/java/sinnet/ws/GreetingResource.java
+++ uservice-webapi/main/host/src/main/java/sinnet/ws/GreetingResource.java
@@ -5,12 +5,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+/** To delete. */
 @Path("/hello")
 public class GreetingResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
-    }
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  public String hello() {
+    return "Hello RESTEasy";
+  }
 }
