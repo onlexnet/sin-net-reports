6f8314ed94cba4a7b60c89616071bb2dfb6e6c97
Sławomir Siudek <slawomir@siudek.net>
2022-12-17 14:04:30 +0100

    Startable host, failing tests

--- service-postgres/scripts/3_init_uservice_projects.sql
+++ service-postgres/scripts/3_init_uservice_projects.sql
@@ -1,2 +1,2 @@
-CREATE ROLE uservice_dbuser WITH LOGIN PASSWORD 'uservice_dbuser';
-CREATE SCHEMA uservice_projects AUTHORIZATION uservice_dbuser;
+CREATE ROLE uservice_projects_role_name WITH LOGIN PASSWORD 'uservice_projects_role_password';
+CREATE SCHEMA uservice_projects AUTHORIZATION uservice_projects_role_name;
