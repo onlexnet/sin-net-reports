d42d9136dbd42d1acefe933a6d06d9b1d991ef56
Sławomir Siudek <slawomir@siudek.net>
2023-02-15 21:15:58 +0100

    Checkstyle

--- uservice-webapi/main/host/src/main/java/sinnet/ws/Report2Controller.java
+++ uservice-webapi/main/host/src/main/java/sinnet/ws/Report2Controller.java
@@ -39,23 +39,23 @@ public class Report2Controller implements TimeentriesMapper {
   @Path("/2/{projectId}")
   @Produces("application/pdf")
   public Uni<Response> downloadPdfFile(UUID projectId,
-    @QueryParam("yearFrom") int yearFrom, @QueryParam("monthFrom") int monthFrom,
-    @QueryParam("yearTo") int yearTo, @QueryParam("monthTo") int monthTo) {
+      @QueryParam("yearFrom") int yearFrom, @QueryParam("monthFrom") int monthFrom,
+      @QueryParam("yearTo") int yearTo, @QueryParam("monthTo") int monthTo) {
 
     var dateFrom = LocalDate.of(yearFrom, monthFrom, 1);
     var dateTo = LocalDate.of(yearTo, monthTo, 1).plusMonths(1).minusDays(1);
     var projectIdAsString = projectId.toString();
 
 
-     return getTimeentries(projectIdAsString, dateFrom, dateTo)
+    return getTimeentries(projectIdAsString, dateFrom, dateTo)
       .flatMap(it -> {
         var entries = it;
         var reportRequest = asReportRequest(entries);
         return reportsClient.produce(reportRequest);
       })
       .map(it -> {
-         var result = it.getData().toByteArray();
-         return Response.ok(result)
+        var result = it.getData().toByteArray();
+        return Response.ok(result)
           .header("Cache-Control", "no-cache, no-store, must-revalidate")
           .header("Content-Disposition", "inline; filename=report " + yearFrom + "-" + monthFrom + ".pdf")
           .header("Expires", "0")
@@ -68,24 +68,24 @@ public class Report2Controller implements TimeentriesMapper {
   // TODO move such aggregation logic close to data service
   ReportRequest asReportRequest(List<TimeEntryModel> items) {
     var map = items
-      .map(it -> Tuple.of(it.getServicemanName(),YearMonth.from(it.getWhen()), it.getHowLong(), it.getHowFar()))
-      .map(it -> {
-        var key = Tuple.of(it._1, it._2);
-        return Tuple.of(key, Tuple.of(it._3, it._4));
-      })
-      .foldLeft(
-        HashMap.<Tuple2<String, YearMonth>, Tuple2<Integer, Integer>>empty(),
-        (acc, v) -> acc.put(v._1, v._2, (v1, v2) -> Tuple.of(v1._1 + v2._1, v1._2 + v2._2)));
+        .map(it -> Tuple.of(it.getServicemanName(),YearMonth.from(it.getWhen()), it.getHowLong(), it.getHowFar()))
+        .map(it -> {
+          var key = Tuple.of(it._1, it._2);
+          return Tuple.of(key, Tuple.of(it._3, it._4));
+        })
+        .foldLeft(
+          HashMap.<Tuple2<String, YearMonth>, Tuple2<Integer, Integer>>empty(),
+          (acc, v) -> acc.put(v._1, v._2, (v1, v2) -> Tuple.of(v1._1 + v2._1, v1._2 + v2._2)));
 
-      var requestBuilder = ReportRequest.newBuilder();
-      map.forEach((k, v) -> requestBuilder.addDetailsBuilder()
-          .setPersonName(k._1 + "")
-          .setYearMonth(newBuilder().setYear(k._2.getYear()).setMonth(k._2.getMonthValue()))
-          .setHowLongInMins(v._1)
-          .setHowFarInKms(v._2)
-          .build());
-      return requestBuilder.build();
-    }
+    var requestBuilder = ReportRequest.newBuilder();
+    map.forEach((k, v) -> requestBuilder.addDetailsBuilder()
+        .setPersonName(k._1 + "")
+        .setYearMonth(newBuilder().setYear(k._2.getYear()).setMonth(k._2.getMonthValue()))
+        .setHowLongInMins(v._1)
+        .setHowFarInKms(v._2)
+        .build());
+    return requestBuilder.build();
+  }
 
   @Value 
   class TimeEntryModel {
