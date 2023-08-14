b22480b33c37556dde4f7e2fb9324113f2e48297
siudeks <slawomir@siudek.net>
2023-08-14 19:41:30 +0200

    webapi CI

--- uservice-timeentries/main/prerequisits.sh
+++ uservice-timeentries/main/prerequisits.sh
@@ -1,2 +1,3 @@
+sdk install mvnd
 mvn clean install -f ../../libs-java/ -ntp
 mvn clean install -f ../../api/client-java/ -ntp
---
+++ uservice-webapi/prerequisits.sh
@@ -0,0 +1,3 @@
+sdk install mvnd
+mvnd clean install -f ../libs-java/ -ntp
+mvnd clean install -f ../api/client-java/ -ntp
