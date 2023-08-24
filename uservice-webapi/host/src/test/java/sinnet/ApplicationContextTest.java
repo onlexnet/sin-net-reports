9af1a292d17d5cbdb1e68d6dab5b59b9ea638a87
siudeks <slawomir@siudek.net>
2023-08-24 16:23:25 +0000

    All builds are green

--- uservice-webapi/host/src/test/java/sinnet/ApplicationContextTest.java
+++ uservice-webapi/host/src/test/java/sinnet/ApplicationContextTest.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles(profiles = "test")
+@SpringBootTest(
+  webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+  properties = { "DAPR_GRPC_PORT=0" }, classes = { Program.class})
+
+@ActiveProfiles(profiles = Profiles.App.DEV)
 class ApplicationContextTest {
 
   @Test
---
+++ uservice-webapi/host/src/test/java/sinnet/SecurityTest.java
@@ -0,0 +1,34 @@
+package sinnet;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+import java.util.UUID;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+import onlexnet.sinnet.webapi.test.AppApi;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT, classes = Program.class)
+@ActiveProfiles(Profiles.App.TEST)
+class GqlTest {
+
+  @Autowired
+  TestRestTemplate restTemplate;
+
+  AppApi appApi;
+
+  @Before
+  public void before() {
+    var requestorEmail = "email@" + UUID.randomUUID();
+    appApi = new AppApi(restTemplate.getRootUri(), requestorEmail);
+  }
+
+  @Test
+  void doSomething() {
+  }
+}
