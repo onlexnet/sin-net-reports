cd7d9831349067cf9694a5f16b3bae2f7abb756e
siudeks <slawomir@siudek.net>
2023-10-04 17:17:08 +0000

    Report2

--- uservice-webapi/host/src/main/java/sinnet/ws/Report2Controller.java
+++ uservice-webapi/host/src/main/java/sinnet/ws/Report2Controller.java
@@ -9,6 +9,7 @@ import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
