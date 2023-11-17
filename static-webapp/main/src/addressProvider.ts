40c51001fc550ba2e4a8b818b1f133938c314d73
siudeks <slawomir@siudek.net>
2023-11-17 23:12:03 +0000

    ardcode backend address

--- static-webapp/main/src/addressProvider.ts
+++ static-webapp/main/src/addressProvider.ts
@@ -5,13 +5,12 @@ export const addressProvider = (): { host: string } => {
 }
 
 const asBackendHost = (appHost: string): string => {
-  const dev01 = "https://sinnet-webapi.proudstone-d1ff7602.westeurope.azurecontainerapps.io";
+  const prd01 = "https://sinnet-webapi.wonderfulriver-536309a1.westeurope.azurecontainerapps.io";
+
   console.log("origin", origin);
 
   if (origin.endsWith("raport.sin.net.pl")) return appHost;
-  if (origin.includes("localhost")) return dev01;
-  if (origin.endsWith("sinnet.local")) return appHost;
-  if (origin.endsWith("sinnet-dev01.onlex.net")) return dev01;
+  if (origin.includes("localhost")) return prd01;
+  if (origin.endsWith("sinnet.onlex.net")) return prd01;
   return "https://sinnet.local";
-  // return "http://localhost:11010";
 }
