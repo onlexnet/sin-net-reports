2602beb3083ffdafc61999928eccb0e3335d950d
siudeks <slawomir@siudek.net>
2024-05-14 09:43:04 +0000

    Fix tests

--- uservice-webapi/host/src/test/java/sinnet/bdd/TotpTest.java
+++ uservice-webapi/host/src/test/java/sinnet/bdd/TotpTest.java
@@ -1,6 +1,9 @@
 package sinnet.bdd;
 
+import java.time.Clock;
 import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
@@ -12,10 +15,9 @@ import com.bastiaanjansen.otp.TOTPGenerator;
 
 import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 public class TotpTest {
 
-  static String secret = "JERAN5FEJNCN7C44YJCXAG6CI3MGPDNK63N3WPYKAVYUMHQ4FKP5VDOU6VLGU6PR";
+  static String exampleSecret = "JERAN5FEJNCN7C44YJCXAG6CI3MGPDNK63N3WPYKAVYUMHQ4FKP5VDOU6VLGU6PR";
 
 
   @Test
@@ -25,15 +27,15 @@ public class TotpTest {
       .of(2001,2,3,4,5,6)
       .toInstant(ZoneOffset.UTC);
 
-    TOTPGenerator totp = new TOTPGenerator.Builder(secret.getBytes())
+    TOTPGenerator totp = new TOTPGenerator.Builder(exampleSecret.getBytes())
         .withHOTPGenerator(builder -> {
           builder.withPasswordLength(6);
           builder.withAlgorithm(HMACAlgorithm.SHA1); // SHA256 and SHA512 are also supported
         })
         .withPeriod(Duration.ofSeconds(30))
+        .withClock(Clock.fixed(Instant.from(when), ZoneOffset.UTC))
         .build();
     
     Assertions.assertThat(totp.now()).isEqualTo("135172");
   }
-
 }
