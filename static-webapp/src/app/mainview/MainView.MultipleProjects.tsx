18a8b42332c0636433a2085a1f40404b14e5c995
siudeks <slawomir@siudek.net>
2025-01-11 20:57:40 +0000

    Formatting

--- static-webapp/src/app/mainview/MainView.MultipleProjects.tsx
+++ static-webapp/src/app/mainview/MainView.MultipleProjects.tsx
@@ -3,11 +3,11 @@ import { Radio, Button, Space } from "antd";
 import _ from "lodash";
 
 interface MainViewMultipleProjectsProps {
-    projects: { id: string, name: string }[]
-    projectSelected: (id: string) => void
+    projects: { id: string, name: string }[];
+    projectSelected: (id: string) => void;
 }
-export const MainViewMultipleProjects: React.FC<MainViewMultipleProjectsProps> = props => {
 
+export const MainViewMultipleProjects: React.FC<MainViewMultipleProjectsProps> = props => {
     const options = _.chain(props.projects)
         .map(it => ({ label: it.name, value: it.id }))
         .value();
@@ -16,17 +16,21 @@ export const MainViewMultipleProjects: React.FC<MainViewMultipleProjectsProps> =
 
     return (
         <Space direction="vertical" align="center">
-            <Radio.Group onChange={e => setSelected(e.target.value)}>
-                <Space direction="vertical">
-                    {options.map(option => (
-                        <Radio key={option.value} value={option.value}>
-                            {option.label}
-                        </Radio>
-                    ))}
-                </Space>
-            </Radio.Group>
-            <Button type="primary" disabled={!selected} onClick={() => props.projectSelected(selected!)}>Kontunuuj pracę z wybranym projektem</Button>
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'start' }}>
+                <Radio.Group onChange={e => setSelected(e.target.value)} style={{ marginBottom: '16px', marginTop: '16px' }} value={selected}>
+                    <Space direction="vertical" align="start"> 
+                        {options.map(option => (
+                            <Radio key={option.value} value={option.value}>
+                                {option.label}
+                            </Radio>
+                        ))}
+                    </Space>
+                </Radio.Group>
+                <Button type="primary" disabled={!selected} onClick={() => props.projectSelected(selected!)}>
+                    Kontunuuj pracę z wybranym projektem
+                </Button>
+            </div>
         </Space>
     );
-}
+};
 
