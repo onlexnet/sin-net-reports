8912aafff1a0d6106f17bd0b2a9eb4bb81066b60
siudeks <slawomir@siudek.net>
2025-01-16 21:35:49 +0000

    Fix domain check type

--- infra/shared/module_static_webapp_custom_domain/main.tf
+++ infra/shared/module_static_webapp_custom_domain/main.tf
@@ -2,7 +2,7 @@
 resource "azurerm_static_web_app_custom_domain" "main" {
   static_web_app_id = var.static_web_app_id
   domain_name       = var.domain_name
-  validation_type   = "dns-txt-token"
+  validation_type   = "cname-delegation"
 }
 
 # resource "azurerm_static_site_custom_domain" "sinnet" {
