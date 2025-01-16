9256cddbf740de49090df657717f00261e46eb3a
siudeks <slawomir@siudek.net>
2025-01-16 21:48:09 +0000

    Proper token of webapp-test

--- infra/shared/module_github/main.tf
+++ infra/shared/module_github/main.tf
@@ -15,17 +15,17 @@ resource "github_repository_environment" "main" {
 
   deployment_branch_policy {
     # Whether only branches with branch protection rules can deploy to this environment.
-    protected_branches     = false
-    
+    protected_branches = false
+
     custom_branch_policies = true
   }
 }
 
 resource "github_repository_environment_deployment_policy" "main" {
-  repository     = data.github_repository.sinnet.name
-  environment    = github_repository_environment.main.environment
+  repository  = data.github_repository.sinnet.name
+  environment = github_repository_environment.main.environment
   # simple single name just to allow me deploy secondary version of WebApp
-  branch_pattern = "webapp-test" 
+  branch_pattern = "webapp-test"
 }
 
 resource "github_actions_environment_secret" "azure_static_web_apps_api_token" {
@@ -39,7 +39,7 @@ resource "github_actions_environment_secret" "webapp_test_api_token" {
   environment     = github_repository_environment.main.environment
   repository      = data.github_repository.sinnet.name
   secret_name     = "WEBAPP_TEST_API_TOKEN"
-  plaintext_value = var.azure_static_web_apps_api_token
+  plaintext_value = var.webapp_test_api_token
 }
 
 resource "github_actions_environment_secret" "ONLEXNET_TENANT_ID" {
