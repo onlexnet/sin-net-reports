687e47c32f9bb24aa523678b725b7bd83c375084
siudeks <slawomir@siudek.net>
2025-01-25 20:14:58 +0000

    Icons

--- static-webapp/src/NavBar.tsx
+++ static-webapp/src/NavBar.tsx
@@ -3,16 +3,23 @@ import { RouteComponentProps } from "react-router-dom";
 import { routing } from './Routing';
 import _ from 'lodash';
 import { Menu } from 'antd';
+import { HomeOutlined, UserOutlined, BugOutlined } from '@ant-design/icons';
 
 interface NavBarProps extends RouteComponentProps<any> { }
 
 export const NavBar: React.FC<NavBarProps> = (props) => {
   return (
-    <Menu mode='vertical'>
-      <Menu.Item key='home' onClick={() => props.history.push(routing.actions)}>Usługi</Menu.Item>
-      <Menu.Item key='customers' onClick={() => props.history.push(routing.customers)}>Klienci</Menu.Item>
-      <Menu.Item key='debug' onClick={() => props.history.push(routing.debug)}>Debug</Menu.Item>
-    </Menu>
+    <div style={{ height: '100vh', backgroundColor: '#001529', color: '#fff' }}>
+      <Menu
+        mode='vertical'
+        theme='light'
+        style={{ height: '100%', borderRight: 0 }}
+      >
+        <Menu.Item key='home' icon={<HomeOutlined />} onClick={() => props.history.push(routing.actions)}>Usługi</Menu.Item>
+        <Menu.Item key='customers' icon={<UserOutlined />} onClick={() => props.history.push(routing.customers)}>Klienci</Menu.Item>
+        <Menu.Item key='debug' icon={<BugOutlined />} onClick={() => props.history.push(routing.debug)}>Debug</Menu.Item>
+      </Menu>
+    </div>
   );
 };
 
