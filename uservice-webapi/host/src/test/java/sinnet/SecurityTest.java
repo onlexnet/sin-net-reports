ac42943800c11cd60a616d42b3cf56c0ca45edcd
siudeks <slawomir@siudek.net>
2025-02-13 21:11:45 +0000

    Remove unintended changes

--- uservice-webapi/host/src/test/java/sinnet/SecurityTest.java
+++ uservice-webapi/host/src/test/java/sinnet/SecurityTest.java
@@ -2,13 +2,7 @@ package sinnet;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.UUID;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.StreamSupport;
 
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
@@ -36,12 +30,5 @@ class GqlTest {
 
   @Test
   void doSomething() {
-    var ar = new int[] {1, 2, 3, 4, 4, 1 ,1 };
-    var aaa1 = IntStream.of(ar).collect(
-      () -> new HashMap<Integer, Integer>(),
-      (acc, i) -> { 
-        acc.compute(i, (k, v) -> v == null ? i : v + i);  },
-      (i1, i2) -> { });
-    
   }
 }
