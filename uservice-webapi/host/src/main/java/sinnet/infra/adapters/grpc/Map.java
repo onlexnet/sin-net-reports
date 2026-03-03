f2ae9ca64d17c829627b78277b67397e33a44765
siudeks <slawomir@siudek.net>
2026-03-03 22:05:42 +0000

    Less annotations

--- uservice-webapi/host/src/main/java/sinnet/infra/adapters/grpc/Map.java
+++ uservice-webapi/host/src/main/java/sinnet/infra/adapters/grpc/Map.java
@@ -1,5 +1,10 @@
 package sinnet.infra.adapters.grpc;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
@@ -10,25 +15,26 @@ import sinnet.domain.models.UserToken;
 
 @Mapper(unmappedTargetPolicy = ReportingPolicy.ERROR, unmappedSourcePolicy = ReportingPolicy.ERROR)
 public interface Map {
-    
-    static final Map apply = Mappers.getMapper(Map.class);
 
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.CLASS)
     @Mapping(target = "mergeFrom", ignore = true)
     @Mapping(target = "clearField", ignore = true)
     @Mapping(target = "clearOneof", ignore = true)
     @Mapping(target = "unknownFields", ignore = true)
     @Mapping(target = "mergeUnknownFields", ignore = true)
     @Mapping(target = "allFields", ignore = true)
+    @interface IgnoreGrpcMetaFields {
+    }
+    
+    static final Map apply = Mappers.getMapper(Map.class);
+
+    @IgnoreGrpcMetaFields
     @Mapping(target = "projectIdBytes", ignore = true)
     @Mapping(target = "requestorEmailBytes", ignore = true)
     sinnet.grpc.common.UserToken map(UserToken it);
 
-    @Mapping(target = "mergeFrom", ignore = true)
-    @Mapping(target = "clearField", ignore = true)
-    @Mapping(target = "clearOneof", ignore = true)
-    @Mapping(target = "unknownFields", ignore = true)
-    @Mapping(target = "mergeUnknownFields", ignore = true)
-    @Mapping(target = "allFields", ignore = true)
+    @IgnoreGrpcMetaFields
     @Mapping(target = "projectIdBytes", ignore = true)
     @Mapping(target = "entityIdBytes", ignore = true)
     @Mapping(target = "entityId", source = "id")
