777d62e06bea2f19c3a4b9af61bc3a31f069c24d
siudeks <slawomir@siudek.net>
2026-03-14 09:20:33 +0000

    Introduce CustomerReserveCommand and separate layers

--- uservice-webapi/host/src/main/java/sinnet/app/ports/in/CustomersPortIn.java
+++ uservice-webapi/host/src/main/java/sinnet/app/ports/in/CustomersPortIn.java
@@ -1,6 +1,7 @@
 package sinnet.app.ports.in;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.function.Function;
 
 import sinnet.app.flow.request.CustomerGetQuery;
@@ -8,11 +9,11 @@ import sinnet.app.flow.request.CustomerGetResult;
 import sinnet.app.flow.request.CustomerListQuery;
 import sinnet.app.flow.request.CustomerListResult;
 import sinnet.app.flow.request.CustomerRemoveCommand;
+import sinnet.app.flow.request.CustomerReserveCommand;
 import sinnet.app.flow.request.CustomerUpdateCommand;
 import sinnet.gql.models.CustomerEntityGql;
 import sinnet.grpc.customers.RemoveReply;
 import sinnet.grpc.customers.ReserveReply;
-import sinnet.grpc.customers.ReserveRequest;
 import sinnet.grpc.customers.UpdateResult;
 
 public interface CustomersPortIn {
@@ -24,7 +25,7 @@ public interface CustomersPortIn {
     
     CustomerGetResult get(CustomerGetQuery query);
     
-    ReserveReply reserve(ReserveRequest request);
+    ReserveReply reserve(CustomerReserveCommand projectId);
 
     <T> List<T> customerList(String projectId, String requestorEmail, Function<sinnet.grpc.customers.CustomerModel, T> mapper);
 
--- uservice-webapi/host/src/main/java/sinnet/gql/api/CustomersMutationReserve.java
+++ uservice-webapi/host/src/main/java/sinnet/gql/api/CustomersMutationReserve.java
@@ -4,6 +4,7 @@ import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 import lombok.RequiredArgsConstructor;
+import sinnet.app.flow.request.CustomerReserveCommand;
 import sinnet.app.ports.in.CustomersPortIn;
 import sinnet.infra.adapters.gql.EntityGqlMapper;
 import sinnet.infra.adapters.grpc.EntityGrpcMapper;
@@ -20,10 +21,8 @@ class CustomersMutationReserve {
 
   @SchemaMapping
   public EntityGql reserve(CustomersMutation self) {
-    var request = ReserveRequest.newBuilder()
-        .setProjectId(self.projectId().toString())
-        .build();
-    var result = service.reserve(request);
+    var cmd = new CustomerReserveCommand(self.projectId());
+    var result = service.reserve(cmd);
     return entityGqlMapper.toGql(entityGrpcMapper.fromGrpc(result.getEntityId()));
   }
 }
