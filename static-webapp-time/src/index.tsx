d31e47126aaaf49c83da0b8d927c83311d899e89
siudeks <slawomir@siudek.net>
2026-03-18 19:15:34 +0000

    TKT-01

--- static-webapp-time/src/index.tsx
+++ static-webapp-time/src/index.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import * as serviceWorker from "./serviceWorker";
+import "./index.css";
 
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import App from "./app/App";
 
-import { ConfigProvider, theme } from "antd";
+import { Toaster } from "sonner";
 import { initializeApplicationInsights } from "./app/configuration/ApplicationInsights";
 import { loadRuntimeConfig } from "./app/configuration/RuntimeConfig";
-
-const lightToken = theme.getDesignToken({ algorithm: theme.defaultAlgorithm }); 
 
 // Initialize application
 const initializeApp = async () => {
@@ -29,11 +28,10 @@ const initializeApp = async () => {
 
     const root = createRoot(appElement);
     root.render(
-      <ConfigProvider theme={{ token: lightToken }}>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </ConfigProvider>
+      <Provider store={store}>
+        <App />
+        <Toaster richColors position="top-right" />
+      </Provider>
     );
   } catch (error) {
     console.error('Failed to initialize application:', error);
